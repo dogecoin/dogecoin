637daea4221d88ba64b7396b1eacd44b06f78f9a
XNL JFR <root@securitydot.net>
2013-12-17 18:09:07 +0200

    raise sending limit to 500000000
    raise sending limit to 500000000

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -137,7 +137,7 @@ double GetDifficulty(const CBlockIndex* blockindex = NULL)
 int64 AmountFromValue(const Value& value)
 {
     double dAmount = value.get_real();
-    if (dAmount <= 0.0 || dAmount > 84000000.0)
+    if (dAmount <= 0.0 || dAmount > 500000000.0)
         throw JSONRPCError(-3, "Invalid amount");
     int64 nAmount = roundint64(dAmount * COIN);
     if (!MoneyRange(nAmount))
