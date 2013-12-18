3e5edeb2aeed2f1d605b8b59557a1d1d8d22d249
XNL JFR <root@securitydot.net>
2013-12-18 16:38:12 +0200

    add first dnsseed for doge
    add first dnsseed for doge

--- src/net.cpp
+++ src/net.cpp
@@ -1150,6 +1150,8 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
+       // default hypernode dns seed , change later when network grows
+       {"seed.dogeftw.com"},
     // {"dogecoinpool.org", "dnsseed.dogecoinpool.org"},
     // {"bytesized-vps.com", "dnsseed.bytesized-vps.com"},
     // {"xurious.com", "dnsseed.ltc.xurious.com"},
