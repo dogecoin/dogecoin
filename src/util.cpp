27093686eab31c27b773d67f8a411238ee4f5abd
Spacefish <timo.witte@gmail.com>
2013-12-23 19:55:28 +0100

    Change Linkcoin to DogeCoin in clock warning

--- src/util.cpp
+++ src/util.cpp
@@ -1207,7 +1207,7 @@ void AddTimeData(const CNetAddr& ip, int64 nTime)
                 if (!fMatch)
                 {
                     fDone = true;
-                    string strMessage = _("Warning: Please check that your computer's date and time are correct.  If your clock is wrong Linkcoin will not work properly.");
+                    string strMessage = _("Warning: Please check that your computer's date and time are correct.  If your clock is wrong DogeCoin will not work properly.");
                     strMiscWarning = strMessage;
                     printf("*** %s\n", strMessage.c_str());
                     uiInterface.ThreadSafeMessageBox(strMessage+" ", string("DogeCoin"), CClientUIInterface::OK | CClientUIInterface::ICON_EXCLAMATION);
