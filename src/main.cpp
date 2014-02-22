5e2d98a11a7b3c8b5f80292685cf5840bd7d6de6
xnljfr <root@securitydot.net>
2014-02-22 21:05:37 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2494,7 +2494,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
         CAddress addrFrom;
         uint64 nNonce = 1;
         vRecv >> pfrom->nVersion >> pfrom->nServices >> nTime >> addrMe;
-        if (pfrom->nVersion < MIN_PROTO_VERSION)
+        if (pfrom->nVersion < MIN_PEER_PROTO_VERSION)
         {
             // Since February 20, 2012, the protocol is initiated at version 209,
             // and earlier versions are no longer supported
