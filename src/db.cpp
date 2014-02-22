2ecf9c0e4ce31456e90d1b0a1cc6a19f4ca07f8f
xnljfr <root@securitydot.net>
2014-02-22 10:39:44 +0200

    1.4 Fork fix
    1.4 Fork fix

--- src/db.cpp
+++ src/db.cpp
@@ -84,7 +84,7 @@ bool CDBEnv::Open(boost::filesystem::path pathEnv_)
     dbenv.set_cachesize(nDbCache / 1024, (nDbCache % 1024)*1048576, 1);
     dbenv.set_lg_bsize(1048576);
     dbenv.set_lg_max(10485760);
-    dbenv.set_lk_max_locks(10000);
+    dbenv.set_lk_max_locks(537000);
     dbenv.set_lk_max_objects(10000);
     dbenv.set_errfile(fopen(pathErrorFile.string().c_str(), "a")); /// debug
     dbenv.set_flags(DB_AUTO_COMMIT, 1);
