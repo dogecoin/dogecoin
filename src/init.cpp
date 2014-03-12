cff8ddb8f2120208774c90a0d844854e81f4ddd9
Michagogo <Michagogo@users.noreply.github.com>
2014-03-12 22:14:11 +0200

    Adjust branding in datadir lock error message Conflicts:
    	src/init.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -594,7 +594,7 @@ bool AppInit2(boost::thread_group& threadGroup)
     if (file) fclose(file);
     static boost::interprocess::file_lock lock(pathLockFile.string().c_str());
     if (!lock.try_lock())
-        return InitError(strprintf(_("Cannot obtain a lock on data directory %s. Dogecoin is probably already running."), strDataDir));
+        return InitError(strprintf(_("Cannot obtain a lock on data directory %s. Dogecoin Core is probably already running."), strDataDir));
 
     if (GetBoolArg("-shrinkdebugfile", !fDebug))
         ShrinkDebugFile();
