c805f0b799df689954db2b829da8cce7a797a5c0
Wladimir J. van der Laan <laanwj@gmail.com>
2014-03-22 10:22:42 +0100

    qt: Do proper boost::path conversion
    Convert from QString unicode from/to the OS-dependent locale
    as used by boost::filesystem::path as needed.
    
    Solves #3916.

--- src/qt/guiutil.h
+++ src/qt/guiutil.h
@@ -9,6 +9,8 @@
 #include <QObject>
 #include <QString>
 
+#include <boost/filesystem.hpp>
+
 class QValidatedLineEdit;
 class SendCoinsRecipient;
 
@@ -123,6 +125,12 @@ namespace GUIUtil
     void saveWindowGeometry(const QString& strSetting, QWidget *parent);
     /** Restore window size and position */
     void restoreWindowGeometry(const QString& strSetting, const QSize &defaultSizeIn, QWidget *parent);
+
+    /* Convert QString to OS specific boost path through UTF-8 */
+    boost::filesystem::path qstringToBoostPath(const QString &path);
+
+    /* Convert OS specific boost path to QString through UTF-8 */
+    QString boostPathToQString(const boost::filesystem::path &path);
 
 } // namespace GUIUtil
 
