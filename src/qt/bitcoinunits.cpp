441147855b8caacb097d25799d3cda9f061bd66e
Wladimir J. van der Laan <laanwj@gmail.com>
2014-03-22 08:26:50 +0100

    qt: use QChar(0x03BC) for mu symbol
    Embedding UTF-8 in source code is not portable.
    
    Also make unit descriptions translatable.

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -42,7 +42,7 @@ QString BitcoinUnits::name(int unit)
     {
     case BTC: return QString("DOGE");
     case mBTC: return QString("mDOGE");
-    case uBTC: return QString::fromUtf8("μDOGE");
+    case uBTC: return QChar(0x03BC) + QString("DOGE");
     default: return QString("???");
     }
 }
@@ -51,9 +51,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Dogecoins");
-    case mBTC: return QString("Milli-Dogecoins (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Micro-Dogecoins (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case BTC: return tr("Dogecoins");
+    case mBTC: return tr("Milli-Dogecoins (1 / 1,000)");
+    case uBTC: return tr("Micro-Dogecoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
