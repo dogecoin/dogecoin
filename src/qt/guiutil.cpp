bitcoin/bitcoin

qt: Use locale-specific number formatting
- Change bitcoinamountfield to use locale-specific number format
- Change bitcoinunits to show and parse locale-specific numbers
- If a language/territory is selected in options, this is set as default
  locale (overrides system locale).

Fixes #3887

src/qt/guiutil.cpp
@@ -58,7 +58,7 @@ namespace GUIUtil {
 
 QString dateTimeStr(const QDateTime &date)
 {
-    return date.date().toString(Qt::SystemLocaleShortDate) + QString(" ") + date.toString("hh:mm");
+    return date.date().toString(Qt::DefaultLocaleShortDate) + QString(" ") + date.toString("hh:mm");
 }
 
 QString dateTimeStr(qint64 nTime)
@@ -133,7 +133,10 @@ bool parseBitcoinURI(const QUrl &uri, SendCoinsRecipient *out)
         {
             if(!i->second.isEmpty())
             {
-                if(!BitcoinUnits::parse(BitcoinUnits::BTC, i->second, &rv.amount))
+                // Parse amount in C locale with no number separators
+                QLocale locale(QLocale::c());
+                locale.setNumberOptions(QLocale::OmitGroupSeparator | QLocale::RejectGroupSeparator);
+                if(!BitcoinUnits::parse(BitcoinUnits::BTC, i->second, &rv.amount, locale))
                 {
                     return false;
                 }

dogecoin/dogecoin

(cherry picked from commit
laanwj/bitcoin@db0b8f3a2e96bc7c7aff95c9ade65be33dd27209,
bitcoin/bitcoin#3893)

src/qt/guiutil.cpp
@@ -58,7 +58,7 @@ namespace GUIUtil {
 
 QString dateTimeStr(const QDateTime &date)
 {
-    return date.date().toString(Qt::SystemLocaleShortDate) + QString(" ") + date.toString("hh:mm");
+    return date.date().toString(Qt::DefaultLocaleShortDate) + QString(" ") + date.toString("hh:mm");
 }
 
 QString dateTimeStr(qint64 nTime)
@@ -133,7 +133,10 @@ bool parseBitcoinURI(const QUrl &uri, SendCoinsRecipient *out)
         {
             if(!i->second.isEmpty())
             {
-                if(!BitcoinUnits::parse(BitcoinUnits::DOGE, i->second, &rv.amount))
+                // Parse amount in C locale with no number separators
+                QLocale locale(QLocale::c());
+                locale.setNumberOptions(QLocale::OmitGroupSeparator | QLocale::RejectGroupSeparator);
+                if(!BitcoinUnits::parse(BitcoinUnits::DOGE, i->second, &rv.amount, locale))
                 {
                     return false;
                 }