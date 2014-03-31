ca285fd2c31afb9909d49cc93a84c70d8a12bdc2
langerhans <max.keller@gmx.com>
2014-03-31 22:25:43 +0200

    Actually update AboutDialog

--- src/qt/utilitydialog.cpp
+++ src/qt/utilitydialog.cpp
@@ -26,7 +26,7 @@ AboutDialog::AboutDialog(QWidget *parent) :
     ui->setupUi(this);
 
     // Set current copyright year
-    ui->copyrightLabel->setText(tr("Copyright") + QString(" &copy; 2009-%1 ").arg(COPYRIGHT_YEAR) + tr("The Bitcoin Core developers"));
+    ui->copyrightLabel->setText(tr("Copyright") + QString(" &copy; 2009-%1 ").arg(COPYRIGHT_YEAR) + tr("The Bitcoin Core developers") + QString("<br>") + tr("Copyright") + QString(" &copy; 2013-%1 ").arg(COPYRIGHT_YEAR) + tr("The Dogecoin developers"));
 }
 
 void AboutDialog::setModel(ClientModel *model)
