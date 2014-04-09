f131c1e486cfca7613f170c8c3da58e9161210e0
langerhans <max.keller@gmx.com>
2014-04-09 21:19:48 +0200

    Revert "Hardcode testnet for first alpha release."
    This reverts commit b16685ca0d0e00424281e0af2f346ff60ace53bf.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -291,7 +291,7 @@ void SelectParams(CChainParams::Network network) {
 
 bool SelectParamsFromCommandLine() {
     bool fRegTest = GetBoolArg("-regtest", false);
-    bool fTestNet = true; // GetBoolArg("-testnet", false);
+    bool fTestNet = GetBoolArg("-testnet", false);
 
     if (fTestNet && fRegTest) {
         return false;
