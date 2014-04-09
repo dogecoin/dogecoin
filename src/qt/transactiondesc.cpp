57c02969f18b1f948fbc5c6abe9179ffab334faa
Ross Nicoll <jrn@jrn.me.uk>
2014-04-09 19:43:01 +0100

    Corrected coin maturity time shown in UI.

--- src/qt/transactiondesc.cpp
+++ src/qt/transactiondesc.cpp
@@ -248,7 +248,12 @@ QString TransactionDesc::toHTML(CWallet *wallet, CWalletTx &wtx, int vout, int u
 
         if (wtx.IsCoinBase())
         {
-            quint32 numBlocksToMaturity = COINBASE_MATURITY +  1;
+            quint32 numBlocksToMaturity = COINBASE_MATURITY + 1;
+
+            if (wtx.GetDepthInMainChain() >= COINBASE_MATURITY_SWITCH)
+            {
+                numBlocksToMaturity = COINBASE_MATURITY_NEW + 1;
+            }
             strHTML += "<br>" + tr("Generated coins must mature %1 blocks before they can be spent. When you generated this block, it was broadcast to the network to be added to the block chain. If it fails to get into the chain, its state will change to \"not accepted\" and it won't be spendable. This may occasionally happen if another node generates a block within a few seconds of yours.").arg(QString::number(numBlocksToMaturity)) + "<br>";
         }
 
