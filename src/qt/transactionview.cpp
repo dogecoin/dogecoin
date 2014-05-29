51f2a42a55e04d028a5d2d9b56c32b381ec21dc6
langerhans <max.keller@gmx.com>
2014-05-29 20:33:38 +0200

    Revert "Cleaned up version of #424" - In favor of the new custom URLs
    This reverts commit a4a8784ce8f514cfbe563ab74d1e972be06accf2.

--- src/qt/transactionview.cpp
+++ src/qt/transactionview.cpp
@@ -30,8 +30,6 @@
 #include <QScrollBar>
 #include <QTableView>
 #include <QVBoxLayout>
-#include <QDesktopServices>
-#include <QUrl>
 
 TransactionView::TransactionView(QWidget *parent) :
     QWidget(parent), model(0), transactionProxyModel(0),
@@ -131,18 +129,14 @@ TransactionView::TransactionView(QWidget *parent) :
     QAction *copyTxIDAction = new QAction(tr("Copy transaction ID"), this);
     QAction *editLabelAction = new QAction(tr("Edit label"), this);
     QAction *showDetailsAction = new QAction(tr("Show transaction details"), this);
-    QAction *viewOnDogechain = new QAction(tr("Show transaction on Dogechain"), this);
 
     contextMenu = new QMenu();
     contextMenu->addAction(copyAddressAction);
     contextMenu->addAction(copyLabelAction);
     contextMenu->addAction(copyAmountAction);
     contextMenu->addAction(copyTxIDAction);
-    contextMenu->addSeparator();
     contextMenu->addAction(editLabelAction);
     contextMenu->addAction(showDetailsAction);
-    contextMenu->addSeparator();
-    contextMenu->addAction(viewOnDogechain);
 
     // Connect actions
     connect(dateWidget, SIGNAL(activated(int)), this, SLOT(chooseDate(int)));
@@ -159,7 +153,6 @@ TransactionView::TransactionView(QWidget *parent) :
     connect(copyTxIDAction, SIGNAL(triggered()), this, SLOT(copyTxID()));
     connect(editLabelAction, SIGNAL(triggered()), this, SLOT(editLabel()));
     connect(showDetailsAction, SIGNAL(triggered()), this, SLOT(showDetails()));
-    connect(viewOnDogechain, SIGNAL(triggered()), this, SLOT(viewOnDogechain()));
 }
 
 void TransactionView::setModel(WalletModel *model)
@@ -387,20 +380,6 @@ void TransactionView::showDetails()
     {
         TransactionDescDialog dlg(selection.at(0));
         dlg.exec();
-    }
-}
-
-
-void TransactionView::viewOnDogechain()
-{
-    QModelIndexList selection = transactionView->selectionModel()->selectedRows();
-    if(!selection.isEmpty())
-    {
-        QString format("http://dogechain.info/tx/");
-        QString munged = selection.at(0).data(TransactionTableModel::TxIDRole).toString();
-        format += munged.left(munged.lastIndexOf("-"));
-        
-        QDesktopServices::openUrl(QUrl(format));
     }
 }
 
