8f1a873fc7a4a50b37d7416ed6dcb0dbf444d890
Wladimir J. van der Laan <laanwj@gmail.com>
2014-06-19 15:08:37 +0200

    Move network-time related functions to timedata.cpp/h
    The network time-offset-mangement functions from util.cpp are moved to
    timedata.(cpp|h). This breaks the dependency of util on netbase.

--- src/util.h
+++ src/util.h
@@ -34,7 +34,6 @@
 #include <boost/filesystem/path.hpp>
 #include <boost/thread.hpp>
 
-class CNetAddr;
 class uint256;
 
 static const int64_t COIN = 100000000;
@@ -196,12 +195,9 @@ uint64_t GetRand(uint64_t nMax);
 uint256 GetRandHash();
 int64_t GetTime();
 void SetMockTime(int64_t nMockTimeIn);
-int64_t GetAdjustedTime();
-int64_t GetTimeOffset();
 long hex2long(const char* hexString);
 std::string FormatFullVersion();
 std::string FormatSubVersion(const std::string& name, int nClientVersion, const std::vector<std::string>& comments);
-void AddTimeData(const CNetAddr& ip, int64_t nTime);
 void runCommand(std::string strCommand);
 
 
