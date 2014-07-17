c118e9d275e4955870aed82f20355c76a85b1c54
Philip Kaufmann <phil.kaufmann@t-online.de>
2014-07-17 22:33:58 +0200

    prevent SOCKET leak in BindListenPort()
    - the call to CloseSocket() is placed after the WSAGetLastError(), because
      a CloseSocket() can trigger an error also, which we don't want for the
      logging in this two cases

--- src/net.cpp
+++ src/net.cpp
@@ -1671,6 +1671,7 @@ bool BindListenPort(const CService &addrBind, string& strError, bool fWhiteliste
         else
             strError = strprintf(_("Unable to bind to %s on this computer (bind returned error %s)"), addrBind.ToString(), NetworkErrorString(nErr));
         LogPrintf("%s\n", strError);
+        CloseSocket(hListenSocket);
         return false;
     }
     LogPrintf("Bound to %s\n", addrBind.ToString());
@@ -1680,6 +1681,7 @@ bool BindListenPort(const CService &addrBind, string& strError, bool fWhiteliste
     {
         strError = strprintf(_("Error: Listening for incoming connections failed (listen returned error %s)"), NetworkErrorString(WSAGetLastError()));
         LogPrintf("%s\n", strError);
+        CloseSocket(hListenSocket);
         return false;
     }
 
