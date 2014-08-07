d606943191e1362727cf035da5a498ce1fb09826
Jeff Garzik <jgarzik@bitpay.com>
2014-08-06 23:58:19 -0400

    Closely track mempool byte total. Add "getmempoolinfo" RPC.
    Goal:  Gain live insight into the mempool.  Groundwork for future work
    that caps mempool size.

--- src/txmempool.h
+++ src/txmempool.h
@@ -69,6 +69,7 @@ private:
     CMinerPolicyEstimator* minerPolicyEstimator;
 
     CFeeRate minRelayFee; // Passed to constructor to avoid dependency on main
+    uint64_t totalTxSize; // sum of all mempool tx' byte sizes
 
 public:
     mutable CCriticalSection cs;
@@ -108,6 +109,11 @@ public:
     {
         LOCK(cs);
         return mapTx.size();
+    }
+    uint64_t GetTotalTxSize()
+    {
+        LOCK(cs);
+        return totalTxSize;
     }
 
     bool exists(uint256 hash)
