1d229e1ef55aa62f162e42965d9c684e517d6629
Ross Nicoll <jrn@jrn.me.uk>
2014-08-28 20:58:45 +0100

    Modified comment to make it clear that pull request refers to Bitcoin Core.

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -132,7 +132,7 @@ bool CWallet::AddCScript(const CScript& redeemScript)
 
 bool CWallet::LoadCScript(const CScript& redeemScript)
 {
-    /* A sanity check was added in pull #3843 to avoid adding redeemScripts
+    /* A sanity check was added in Bitcoin Core pull #3843 to avoid adding redeemScripts
      * that never can be redeemed. However, old wallets may still contain
      * these. Do not add them to the wallet and warn. */
     if (redeemScript.size() > MAX_SCRIPT_ELEMENT_SIZE)
