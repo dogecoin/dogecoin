d9ff1efb5d96a9da0a5e23a6c53b3ae3060fadae
langerhans <max.keller@gmx.com>
2015-01-17 19:22:19 +0100

    Update release type and copyright year

--- src/clientversion.h
+++ src/clientversion.h
@@ -15,11 +15,11 @@
 #define CLIENT_VERSION_BUILD       0
 
 // Set to true for release, false for prerelease or test build
-#define CLIENT_VERSION_IS_RELEASE  false
+#define CLIENT_VERSION_IS_RELEASE  true
 
 // Copyright year (2009-this)
 // Todo: update this when changing our copyright comments in the source
-#define COPYRIGHT_YEAR 2014
+#define COPYRIGHT_YEAR 2015
 
 #endif //HAVE_CONFIG_H
 
