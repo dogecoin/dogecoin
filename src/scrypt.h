a6d7745217f62291bf9c6f1a8aa592f0f1556d52
stolen data <djinn@stolendata.net>
2015-01-29 17:17:51 +0100

    Fix conflicting declarations with Boost on FreeBSD

--- src/scrypt.h
+++ src/scrypt.h
@@ -29,14 +29,14 @@ void
 PBKDF2_SHA256(const uint8_t *passwd, size_t passwdlen, const uint8_t *salt,
     size_t saltlen, uint64_t c, uint8_t *buf, size_t dkLen);
 
-static inline uint32_t le32dec(const void *pp)
+static inline uint32_t scrypt_le32dec(const void *pp)
 {
         const uint8_t *p = (uint8_t const *)pp;
         return ((uint32_t)(p[0]) + ((uint32_t)(p[1]) << 8) +
             ((uint32_t)(p[2]) << 16) + ((uint32_t)(p[3]) << 24));
 }
 
-static inline void le32enc(void *pp, uint32_t x)
+static inline void scrypt_le32enc(void *pp, uint32_t x)
 {
         uint8_t *p = (uint8_t *)pp;
         p[0] = x & 0xff;
