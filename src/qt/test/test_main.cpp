9b9e2d49181153f3859f000292d7805168c5d669
Ross Nicoll <jrn@jrn.me.uk>
2015-01-31 12:18:05 +0000

    Revert "qt: Add tests for bitcoin units parsing/formatting"
    This reverts commit 2b6486b2295794773067be60acb60375be2fa8d3.

--- src/qt/test/test_main.cpp
+++ src/qt/test/test_main.cpp
@@ -3,11 +3,10 @@
 #include "bitcoin-config.h"
 #endif
 
-#include "bitcoinunitstests.h"
-#include "uritests.h"
 #ifdef ENABLE_WALLET
 #include "paymentservertests.h"
 #endif
+#include "uritests.h"
 
 #include <QCoreApplication>
 #include <QObject>
@@ -39,9 +38,6 @@ int main(int argc, char *argv[])
     if (QTest::qExec(&test2) != 0)
         fInvalid = true;
 #endif
-    BitcoinUnitsTests test3;
-    if (QTest::qExec(&test3) != 0)
-        fInvalid = true;
 
     return fInvalid;
 }
