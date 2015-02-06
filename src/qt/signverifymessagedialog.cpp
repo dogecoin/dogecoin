c982a1bedd6b4f883060e21cb0d33a5f708de5c1
Sporklin <sporklin@yahoo.com>
2015-02-06 12:13:58 -0500

    [Qt] Example address removed from translation files.

--- src/qt/signverifymessagedialog.cpp
+++ src/qt/signverifymessagedialog.cpp
@@ -27,8 +27,7 @@ SignVerifyMessageDialog::SignVerifyMessageDialog(QWidget *parent) :
 
 #if QT_VERSION >= 0x040700
     ui->signatureOut_SM->setPlaceholderText(tr("Click \"Sign Message\" to generate signature"));
-    ui->addressIn_VM->setPlaceholderText(tr("Enter a Dogecoin address (e.g. DJ7zB7c5BsB9UJLy1rKQtY7c6CQfGiaRLM)"));
-#endif
+  #endif
 
     GUIUtil::setupAddressWidget(ui->addressIn_SM, this);
     GUIUtil::setupAddressWidget(ui->addressIn_VM, this);
