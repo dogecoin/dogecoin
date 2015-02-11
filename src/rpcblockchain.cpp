c0dabcb41d40778eb629faf1dac6cbe31d3cace5
Sporklin <sporklin@yahoo.com>
2015-02-11 17:36:24 -0500

    Replace HexBits in AuxPoW related call.

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -91,7 +91,7 @@ Object blockToJSON(const CBlock& block, const CBlockIndex* blockindex)
         parent_block.push_back(Pair("previousblockhash", block.auxpow->parentBlockHeader.hashPrevBlock.GetHex()));
         parent_block.push_back(Pair("merkleroot", block.auxpow->parentBlockHeader.hashMerkleRoot.GetHex()));
         parent_block.push_back(Pair("time", (boost::int64_t)block.auxpow->parentBlockHeader.nTime));
-        parent_block.push_back(Pair("bits", HexBits(block.auxpow->parentBlockHeader.nBits)));
+        parent_block.push_back(Pair("bits", strprintf("%08x", block.auxpow->parentBlockHeader.nBits)));
         parent_block.push_back(Pair("nonce", (boost::uint64_t)block.auxpow->parentBlockHeader.nNonce));
         auxpow.push_back(Pair("parent_block", Value(parent_block)));
         result.push_back(Pair("auxpow", Value(auxpow)));
