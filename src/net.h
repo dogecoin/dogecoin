fe3a31384c2df823351a10674cbbfc19ae378316
Pieter Wuille <pieter.wuille@gmail.com>
2015-03-05 04:01:22 -0800

    Limit message sizes before transfer
    This introduces a fixed limit for the size of p2p messages, and enforces it
    before download.
    
    Rebased-From: ba04c4a7801e7d68a5e84035b919e5c3626eb7a7
    Github-Pull: #5843

--- src/net.h
+++ src/net.h
@@ -43,6 +43,8 @@ static const int PING_INTERVAL = 2 * 60;
 static const int TIMEOUT_INTERVAL = 20 * 60;
 /** The maximum number of entries in an 'inv' protocol message */
 static const unsigned int MAX_INV_SZ = 50000;
+/** Maximum length of incoming protocol messages (no message over 2 MiB is currently acceptable). */
+static const unsigned int MAX_PROTOCOL_MESSAGE_LENGTH = 2 * 1024 * 1024;
 /** The maximum number of entries in mapAskFor */
 static const size_t MAPASKFOR_MAX_SZ = MAX_INV_SZ;
 
