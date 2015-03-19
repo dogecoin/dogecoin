9b95692530f38781a09f92c16193bbc94d1cdaf1
Pieter Wuille <pieter.wuille@gmail.com>
2015-03-19 09:51:59 -0700

    Always use a 50% chance to choose between tried and new entries
    This change was suggested as Countermeasure 2 in
    Eclipse Attacks on Bitcoin’s Peer-to-Peer Network, Ethan Heilman,
    Alison Kendler, Aviv Zohar, Sharon Goldberg. ePrint Archive Report
    2015/263. March 2015.
    
    Rebased-From: c6a63ceeb4956933588995bcf01dc3095aaeb1fc
    Github-Pull: #5941

--- src/addrman.h
+++ src/addrman.h
@@ -225,8 +225,7 @@ protected:
     void Attempt_(const CService &addr, int64_t nTime);
 
     // Select an address to connect to.
-    // nUnkBias determines how much to favor new addresses over tried ones (min=0, max=100)
-    CAddress Select_(int nUnkBias);
+    CAddress Select_();
 
 #ifdef DEBUG_ADDRMAN
     // Perform consistency check. Returns an error code or zero.
@@ -524,15 +523,16 @@ public:
         }
     }
 
-    // Choose an address to connect to.
-    // nUnkBias determines how much "new" entries are favored over "tried" ones (0-100).
-    CAddress Select(int nUnkBias = 50)
+    /**
+     * Choose an address to connect to.
+     */
+    CAddress Select()
     {
         CAddress addrRet;
         {
             LOCK(cs);
             Check();
-            addrRet = Select_(nUnkBias);
+            addrRet = Select_();
             Check();
         }
         return addrRet;
