6bc02c7060d6cc6ca49f9b20129ac83dc0e92385
ultragtx <ultragtx@gmail.com>
2016-10-21 15:05:36 +0800

    Litecoin: Fix various tests

--- qa/rpc-tests/test_framework/mininode.py
+++ qa/rpc-tests/test_framework/mininode.py
@@ -36,6 +36,7 @@ from threading import RLock
 from threading import Thread
 import logging
 import copy
+import litecoin_scrypt
 from test_framework.siphash import siphash256
 
 BIP0031_VERSION = 60000
@@ -536,6 +537,7 @@ class CBlockHeader(object):
             self.nNonce = header.nNonce
             self.sha256 = header.sha256
             self.hash = header.hash
+            self.scrypt256 = header.scrypt256
             self.calc_sha256()
 
     def set_null(self):
@@ -547,6 +549,7 @@ class CBlockHeader(object):
         self.nNonce = 0
         self.sha256 = None
         self.hash = None
+        self.scrypt256 = None
 
     def deserialize(self, f):
         self.nVersion = struct.unpack("<i", f.read(4))[0]
@@ -557,6 +560,7 @@ class CBlockHeader(object):
         self.nNonce = struct.unpack("<I", f.read(4))[0]
         self.sha256 = None
         self.hash = None
+        self.scrypt256 = None
 
     def serialize(self):
         r = b""
@@ -579,9 +583,11 @@ class CBlockHeader(object):
             r += struct.pack("<I", self.nNonce)
             self.sha256 = uint256_from_str(hash256(r))
             self.hash = encode(hash256(r)[::-1], 'hex_codec').decode('ascii')
+            self.scrypt256 = uint256_from_str(litecoin_scrypt.getPoWHash(r))
 
     def rehash(self):
         self.sha256 = None
+        self.scrypt256 = None
         self.calc_sha256()
         return self.sha256
 
@@ -640,7 +646,7 @@ class CBlock(CBlockHeader):
     def is_valid(self):
         self.calc_sha256()
         target = uint256_from_compact(self.nBits)
-        if self.sha256 > target:
+        if self.scrypt256 > target:
             return False
         for tx in self.vtx:
             if not tx.is_valid():
@@ -652,7 +658,7 @@ class CBlock(CBlockHeader):
     def solve(self):
         self.rehash()
         target = uint256_from_compact(self.nBits)
-        while self.sha256 > target:
+        while self.scrypt256 > target:
             self.nNonce += 1
             self.rehash()
 
@@ -1610,8 +1616,8 @@ class NodeConn(asyncore.dispatcher):
         b"blocktxn": msg_blocktxn
     }
     MAGIC_BYTES = {
-        "mainnet": b"\xf9\xbe\xb4\xd9",   # mainnet
-        "testnet3": b"\x0b\x11\x09\x07",  # testnet3
+        "mainnet": b"\xc0\xc0\xc0\xc0",   # mainnet
+        "testnet3": b"\xfc\xc1\xb7\xdc",  # testnet3
         "regtest": b"\xfa\xbf\xb5\xda",   # regtest
     }
 
