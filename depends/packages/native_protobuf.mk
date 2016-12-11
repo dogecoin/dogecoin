7004e094c76cf0d4d86ab3ea064da3eb1203f722
Patrick Lodder <patricklodder@users.noreply.github.com>
2016-12-11 12:37:21 +0100

    [depends] fetch protobuf from github instead of googlecode

--- depends/packages/native_protobuf.mk
+++ depends/packages/native_protobuf.mk
@@ -1,6 +1,6 @@
 package=native_protobuf
 $(package)_version=2.5.0
-$(package)_download_path=https://protobuf.googlecode.com/files
+$(package)_download_path=https://github.com/google/protobuf/releases/download/v$($(package)_version)
 $(package)_file_name=protobuf-$($(package)_version).tar.bz2
 $(package)_sha256_hash=13bfc5ae543cf3aa180ac2485c0bc89495e3ae711fc6fab4f8ffe90dfb4bb677
 
