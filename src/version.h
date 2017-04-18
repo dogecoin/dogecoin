d30d00a9baf2d0d7644068de7d02987fa665d6f8
Ross Nicoll <jrn@jrn.me.uk>
2017-04-18 13:36:32 +0100

    Replace consensus values with Dogecoin equivalents
    * Replace chain parameters with Dogecoin values
    * Update maximum coins to match Dogecoin
    * Disable version 2 block requirement
    * Update coinbase maturity to match Dogecoin

--- src/version.h
+++ src/version.h
@@ -9,6 +9,8 @@
  * network protocol versioning
  */
 
+// XXX: Decide if this is appropriate - if we reintroduce alerts we may need
+//      to  reduce to 70012
 static const int PROTOCOL_VERSION = 70015;
 
 //! initial proto version, to be increased after version/verack negotiation
@@ -18,7 +20,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 31800;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = GETHEADERS_VERSION;
+static const int MIN_PEER_PROTO_VERSION = 70003;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
