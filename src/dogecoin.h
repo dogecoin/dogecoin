124a1539f8a122952e9b0a346a74fb2f7af06e95
Ross Nicoll <jrn@jrn.me.uk>
2017-04-18 19:15:10 +0100

    Add Dogecoin difficulty calculations

--- src/dogecoin.h
+++ src/dogecoin.h
@@ -2,7 +2,9 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include "chain.h"
 #include "chainparams.h"
 #include "amount.h"
 
 CAmount GetDogecoinBlockSubsidy(int nHeight, const Consensus::Params& consensusParams, uint256 prevHash);
+unsigned int CalculateDogecoinNextWorkRequired(const CBlockIndex* pindexLast, int64_t nLastRetargetTime, const Consensus::Params& params);
