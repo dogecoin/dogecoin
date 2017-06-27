aca95cbbf30d2ea2a0c29122a4bd4afffdfd2b0d
Matt Corallo <git@bluematt.me>
2017-06-27 19:07:52 -0400

    Flush CValidationInterface callbacks prior to destruction
    Note that the CScheduler thread cant be running at this point,
    it has already been stopped with the rest of the init threadgroup.
    Thus, just calling any remaining loose callbacks during Shutdown()
    is sane.
    
    Cherry-picked from: 3192975f1d177aa9f0bbd823c6387cfbfa943610

--- src/test/test_bitcoin.cpp
+++ src/test/test_bitcoin.cpp
@@ -100,6 +100,7 @@ TestingSetup::~TestingSetup()
         UnregisterNodeSignals(GetNodeSignals());
         threadGroup.interrupt_all();
         threadGroup.join_all();
+        GetMainSignals().FlushBackgroundCallbacks();
         GetMainSignals().UnregisterBackgroundSignalScheduler();
         UnloadBlockIndex();
         delete pcoinsTip;
