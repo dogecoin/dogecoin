355a4fb8a6f4774b6dd10a8d5b055528d631585a
Matt Corallo <git@bluematt.me>
2017-12-04 18:31:36 -0500

    Add an interface to get the queue depth out of CValidationInterface
    Cherry-picked from: 5a933cefcc5e0595a1ec46fc5ea287aa163ecd3f

--- src/validationinterface.cpp
+++ src/validationinterface.cpp
@@ -55,6 +55,11 @@ void CMainSignals::FlushBackgroundCallbacks() {
     m_internals->m_schedulerClient.EmptyQueue();
 }
 
+size_t CMainSignals::CallbacksPending() {
+    if (!m_internals) return 0;
+    return m_internals->m_schedulerClient.CallbacksPending();
+}
+
 void CMainSignals::RegisterWithMempoolSignals(CTxMemPool& pool) {
     pool.NotifyEntryRemoved.connect(boost::bind(&CMainSignals::MempoolEntryRemoved, this, boost::placeholders::_1, boost::placeholders::_2));
 }
