f71bd2c2d800ca1cee9aa8d95dd89216e6e3fbb4
Ross Nicoll <jrn@jrn.me.uk>
2017-12-27 21:11:14 +0000

    Change BIP65/66 enforcement to match Dogecoin (#1403)
    * Introduce first estimates at BIP lock-in blocks
    
    * Introduce Dogecoin BIP parameters
    
    * Re-introduce supermajority rules for BIP65
    
    * Add BIP65 supermajority rules
    
    * Tighten v3 block constraints
    
    * Don't enforce coinbase in v2 blocks
    
    * Correct testnet majority params
    
    * Change to using base version when checking supermajority

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -39,11 +39,16 @@ struct BIP9Deployment {
 struct Params {
     uint256 hashGenesisBlock;
     int nSubsidyHalvingInterval;
+    /** Used to check majorities for block version upgrade */
+    int nMajorityEnforceBlockUpgrade;
+    int nMajorityRejectBlockOutdated;
+    int nMajorityWindow;
+    int nCoinbaseMaturity;
     /** Block height and hash at which BIP34 becomes active */
     int BIP34Height;
     uint256 BIP34Hash;
     /** Block height at which BIP65 becomes active */
-    int BIP65Height;
+    // int BIP65Height;
     /** Block height at which BIP66 becomes active */
     int BIP66Height;
     /**
