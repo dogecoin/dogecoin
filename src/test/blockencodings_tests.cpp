8513b8707661c4017194b318132f78e13bf77b33
Max K <langerhans@users.noreply.github.com>
2017-12-28 22:41:29 +0100

    Fix 1.14 unit tests (#1408)
    * Fix BlockEncodings test by setting a legacy block version
    
    * Fix RPC test by adding missing RPC call
    
    * Add missing regtest genesis hashes

--- src/test/blockencodings_tests.cpp
+++ src/test/blockencodings_tests.cpp
@@ -30,9 +30,8 @@ static CBlock BuildBlockTestCase() {
 
     block.vtx.resize(3);
     block.vtx[0] = MakeTransactionRef(tx);
-    block.nVersion = 42;
+    block.nVersion = 1;
     block.hashPrevBlock = InsecureRand256();
-    block.nBits = 0x207fffff;
 
     tx.vin[0].prevout.hash = InsecureRand256();
     tx.vin[0].prevout.n = 0;
@@ -289,7 +288,7 @@ BOOST_AUTO_TEST_CASE(EmptyBlockRoundTripTest)
     CBlock block;
     block.vtx.resize(1);
     block.vtx[0] = MakeTransactionRef(std::move(coinbase));
-    block.nVersion = 42;
+    block.nVersion = 1;
     block.hashPrevBlock = InsecureRand256();
     block.nBits = 0x207fffff;
 
