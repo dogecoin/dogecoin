69886ad3cdc41223826185b618e7a74128757b28
Jim Posen <jimpo@coinbase.com>
2018-01-23 17:25:30 -0800

    blockfilter: Construction of basic block filters.
    Cherry-picked from: c1855f6052aca806fdb51be01b30dfeee8b55f40

--- src/undo.h
+++ src/undo.h
@@ -6,10 +6,11 @@
 #ifndef BITCOIN_UNDO_H
 #define BITCOIN_UNDO_H
 
-#include "compressor.h" 
-#include "consensus/consensus.h"
-#include "primitives/transaction.h"
-#include "serialize.h"
+#include <coins.h>
+#include <compressor.h>
+#include <consensus/consensus.h>
+#include <primitives/transaction.h>
+#include <serialize.h>
 
 /** Undo information for a CTxIn
  *
