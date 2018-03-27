1e0865102db5aef198f94017fbd5c35a13fc842c
Pieter Wuille <pieter.wuille@gmail.com>
2018-03-27 13:34:39 -0700

    Make DummySignatureCreator a singleton
    Cherry-picked from: be67831

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -1049,7 +1049,7 @@ bool CWallet::DummySignTx(CMutableTransaction &txNew, const ContainerType &coins
         const CScript& scriptPubKey = coin.first->tx->vout[coin.second].scriptPubKey;
         SignatureData sigdata;
 
-        if (!ProduceSignature(*this, DummySignatureCreator(), scriptPubKey, sigdata))
+        if (!ProduceSignature(*this, DUMMY_SIGNATURE_CREATOR, scriptPubKey, sigdata))
         {
             return false;
         } else {
