b0cf4ab1928c5ee574a6149a2965ef950337d0e8
Gradinkov <github.gradinkov@outlook.com>
2018-03-27 19:01:47 +0200

    Update OpenSSL to 1.0.2o

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -1,8 +1,8 @@
 package=openssl
-$(package)_version=1.0.1l
+$(package)_version=1.0.2o
 $(package)_download_path=https://www.openssl.org/source
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=b2cf4d48fe5d49f240c61c9e624193a6f232b5ed0baf010681e725963c40d1d4
+$(package)_sha256_hash=ec3f5c9714ba0fd45cb4e087301eb1336c317e0d20b575a125050470e8089e4d
 
 define $(package)_set_vars
 $(package)_config_env=AR="$($(package)_ar)" RANLIB="$($(package)_ranlib)" CC="$($(package)_cc)"
@@ -32,6 +32,7 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
+  $(MAKE) depend && \
   $(MAKE) -j1 build_libs libcrypto.pc libssl.pc openssl.pc
 endef
 
