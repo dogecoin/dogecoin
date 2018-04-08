e37ddc914395f194ab6464f499865ea8a3fcb636
Pieter Wuille <pieter.wuille@gmail.com>
2018-04-08 09:39:05 -0700

    Introduce BigEndian wrapper and use it for netaddress ports

--- src/netaddress.h
+++ src/netaddress.h
@@ -135,7 +135,7 @@ class CSubNet
 class CService : public CNetAddr
 {
     protected:
-        unsigned short port; // host order
+        uint16_t port; // host order
 
     public:
         CService();
@@ -163,13 +163,7 @@ class CService : public CNetAddr
         template <typename Stream, typename Operation>
         inline void SerializationOp(Stream& s, Operation ser_action) {
             READWRITE(ip);
-
-            // TODO: introduce native support for BE serialization in serialize.h
-            unsigned short portN = htons(port);
-            READWRITE(Span<unsigned char>((unsigned char*)&portN, 2));
-            if (ser_action.ForRead()) {
-                 port = ntohs(portN);
-            }
+            READWRITE(WrapBigEndian(port));
         }
 };
 
