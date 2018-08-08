c93ea51273e7fada009c3463fc592105a776a402
Patrick Lodder <patricklodder@users.noreply.github.com>
2018-08-08 11:24:59 -0400

    qa: Add p2p_invalid_locator test
    Adds a custom mininode implementation to test connects and
    disconnects based on the number of allowed locators
    
    Inspired by: fa85c985
    Original Author: MarcoFalke <falke.marco@gmail.com>

--- qa/pull-tester/rpc-tests.py
+++ qa/pull-tester/rpc-tests.py
@@ -128,6 +128,7 @@ testScripts = [
     'rawtransactions.py',
     'reindex.py',
     # vv Tests less than 30s vv
+    'p2p_invalid_locator.py',
     'mempool_resurrect_test.py',
     'txn_doublespend.py --mineblock',
     'txn_clone.py',
