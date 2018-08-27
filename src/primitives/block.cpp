34cbde079f60adb8bcff1a52ee79fb665f07569d
Ross Nicoll <jrn@jrn.me.uk>
2018-08-27 12:34:34 +0100

    Scrypt n=1024 PoW hash
    Scrypt n=1024 PoW hash based upon Colin Percival's Tarnsnap (2009)
    
    Modified by Artforz, coblee, pooler, wtogami, Nikolay Belikov, Adrian Gallagher, Ross Nicoll

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -6,6 +6,7 @@
 #include <primitives/block.h>
 
 #include <hash.h>
+#include <crypto/scrypt.h>
 #include <tinyformat.h>
 #include <utilstrencodings.h>
 #include <crypto/common.h>
@@ -13,6 +14,13 @@
 uint256 CBlockHeader::GetHash() const
 {
     return SerializeHash(*this);
+}
+
+uint256 CBlockHeader::GetPoWHash() const
+{
+    uint256 thash;
+    scrypt_1024_1_1_256(BEGIN(nVersion), BEGIN(thash));
+    return thash;
 }
 
 std::string CBlock::ToString() const
