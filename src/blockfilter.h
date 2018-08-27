6c548c448229f37402e784348499eccf0de76c8f
Jim Posen <jim.posen@gmail.com>
2018-08-27 15:42:35 -0700

    blockfilter: Functions to translate filter types to/from names.
    Cherry-picked from: ba6ff9a6f70139594362b4b4a6b816707bb165c8

--- src/blockfilter.h
+++ src/blockfilter.h
@@ -7,6 +7,7 @@
 
 #include <set>
 #include <stdint.h>
+#include <string>
 #include <vector>
 
 #include <primitives/block.h>
@@ -81,6 +82,12 @@ enum BlockFilterType : uint8_t
     BASIC = 0,
     INVALID = 255,
 };
+
+/** Get the human-readable name for a filter type. Returns empty string for unknown types. */
+const std::string& BlockFilterTypeName(BlockFilterType filter_type);
+
+/** Find a filter type by its human-readable name. */
+bool BlockFilterTypeByName(const std::string& name, BlockFilterType& filter_type);
 
 /**
  * Complete block filter struct as defined in BIP 157. Serialization matches
