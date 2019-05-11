9d8950e9fef1879bf3c9cbc1c704ddefead242b1
Ross Nicoll <jrn@jrn.me.uk>
2019-05-11 22:42:51 +0000

    Replace consensus values with Dogecoin equivalents
    * Replace chain parameters with Dogecoin values
    * Update maximum coins to match Dogecoin
    * Disable version 2 block requirement
    * Update coinbase maturity to match Dogecoin

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -313,7 +313,7 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = Consensus::BIP9Deployment::NO_TIMEOUT;
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = Consensus::BIP9Deployment::ALWAYS_ACTIVE;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = Consensus::BIP9Deployment::NO_TIMEOUT;
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 0; // Disabled
 
         // The best chain should have at least this much work.
         consensus.nMinimumChainWork = uint256S("0x00");
