cb49441a25fd5db6dc177622ebf5b0242348ab20
Patrick Lodder <patricklodder@users.noreply.github.com>
2020-07-22 12:31:13 +0200

    Fix ltc_scrypt dependency for RPC tests
    - travis: RUN_TESTS requires python3-dev. This enables us to
      compile the ltc_scrypt library under python3
    - pull tester deps: use curl instead of wget to reduce dependencies
      as everything else uses curl

--- qa/pull-tester/install-deps.sh
+++ qa/pull-tester/install-deps.sh
@@ -2,7 +2,7 @@
 
 # installs test dependencies
 
-wget https://github.com/langerhans/ltc-scrypt/archive/master.tar.gz
+curl -L https://github.com/langerhans/ltc-scrypt/archive/master.tar.gz --output master.tar.gz
 echo "ade3cdf498927990b6d153d74f0da104114e838584be5a81bef8972accd03341 master.tar.gz" | sha256sum -c
 tar zxf master.tar.gz
 pushd ltc-scrypt-master
