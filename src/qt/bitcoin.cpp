40aa64f3d6d7b5d2459a3a98968b65090fe680dc
barrystyle <barrystyle@westnet.com.au>
2021-02-07 17:28:55 +0800

    build: Update immediately visible remaining bitcoin name references from cli binaries and qt.

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -194,7 +194,7 @@ void BitcoinCore::shutdown()
 }
 
 static int qt_argc = 1;
-static const char* qt_argv = "bitcoin-qt";
+static const char* qt_argv = "dogecoin-qt";
 
 BitcoinApplication::BitcoinApplication():
     QApplication(qt_argc, const_cast<char **>(&qt_argv)),
