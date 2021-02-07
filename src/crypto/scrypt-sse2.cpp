aad77c6e3e81b408ad269b8344c269c2f76437c3
barrystyle <barrystyle@westnet.com.au>
2021-02-07 23:24:11 +0800

    crypto: Add scrypt N=1024 PoW
    Add scrypt pow hashing algorithm as a proof of work function.
    Rebased from litecoin-project/litecoin@02961f0 minus strencodings.h macros.

--- src/crypto/scrypt-sse2.cpp
+++ src/crypto/scrypt-sse2.cpp
@@ -34,7 +34,6 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
-#include <openssl/sha.h>
 
 #include <emmintrin.h>
 
--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -18,7 +18,7 @@ uint256 CBlockHeader::GetHash() const
 uint256 CBlockHeader::GetPoWHash() const
 {
     uint256 thash;
-    scrypt_1024_1_1_256(BEGIN(nVersion), BEGIN(thash));
+    scrypt_1024_1_1_256((char*)&nVersion, (char*)&thash);
     return thash;
 }
 
