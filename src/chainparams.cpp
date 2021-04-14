44bb2a0697bb2aaf82d1d84b347c0964e52c2789
KMSoftware <Twinkykms@gmail.com>
2021-04-14 03:28:55 -0400

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -162,9 +162,12 @@ public:
 
         // Note that of those with the service bits flag, most only support a subset of possible options
         vSeeds.push_back(CDNSSeedData("mint.australiacash.org", "mint.australiacash.org", true));
+        vSeeds.push_back(CDNSSeedData("blockcrushers.xyz", "blockcrushers.xyz", true));
         vSeeds.push_back(CDNSSeedData("45.76.120.169", "45.76.120.169"));
         vSeeds.push_back(CDNSSeedData("139.180.173.59", "139.180.173.59"));
         vSeeds.push_back(CDNSSeedData("165.232.173.117", "165.232.173.117"));
+        vSeeds.push_back(CDNSSeedData("165.232.173.117", "165.232.173.117"));
+        vSeeds.push_back(CDNSSeedData("75.119.143.152", "75.119.143.152"));
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,30);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,22);
