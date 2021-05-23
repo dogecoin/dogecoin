92a1e009c2d8376b3f236ffb349885e0c7098eb4
geekwisdom <braddet@gmail.com>
2021-05-23 13:16:36 -0300

    Rebase for feature #1991 to branch dev-1.21

---
+++ src/dogequirks.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 The Dogecoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+//This file is a little placeholder for 'quirks' that make Dogecoin
+//stand out from other cyprtos
+
+#include<stdio.h>
+
+struct  CDogeQuirks
+{
+static int moon_phase(int y, int m, int d);
+};
