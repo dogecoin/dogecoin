dbaec9c564c894114a30dc2d4ab0e46b12194797
farsider350 <farsider350@github.com>
2021-05-23 18:49:51 +1000

    Update versions

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
 
 // XXX: Decide if this is appropriate - if we reintroduce alerts we may need
 //      to  reduce to 70012
-static const int PROTOCOL_VERSION = 70015;
+static const int PROTOCOL_VERSION = 70025;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -20,7 +20,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 31800;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70003;
+static const int MIN_PEER_PROTO_VERSION = 70015;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
