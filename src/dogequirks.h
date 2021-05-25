a088d40ef4ff51eec43cb0d6a3d52def1a4a5d9a
geekwisdom <braddet@gmail.com>
2021-05-25 07:42:54 -0300

    Fix unit test build error

---
+++ src/dogequirks.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2021 The Dogecoin Core developers
+// Portiosn Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Inspired from Original althorithm posted to voidware.com/moon_phase.htm. 
+// Copyright (c) 2021 The Dogecoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+//This file is a little placeholder for 'quirks' that make Dogecoin
+//stand out from other cyprtos
+
+#include<stdio.h>
+
+struct  CDogeQuirks
+{
+static int moon_phase(int y, int m, int d);
+};
+
