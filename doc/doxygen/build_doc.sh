1019af3655963ed5f18e4aa33012a2e54c160c72
p-j01 <lele.pj01@gmail.com>
2021-06-09 22:59:52 -0400

    doc: Setup theme for documentation

--- doc/doxygen/build_doc.sh
+++ doc/doxygen/build_doc.sh
@@ -1,4 +1,4 @@
 doxygen -g
 sed -i '35s/.*/PROJECT_NAME="Dogecoin"/' Doxyfile
-echo $'INPUT= ../../src ../../ ../\nOUTPUT_DIRECTORY= ./out' >> Doxyfile
+echo $'INPUT= ../../src ../../ ../\nOUTPUT_DIRECTORY= ./out\nGENERATE_TREEVIEW=YES\nHTML_EXTRA_STYLESHEET=theme.css' >> Doxyfile
 doxygen
