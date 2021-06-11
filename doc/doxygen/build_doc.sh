00ab0d3fba30fbd779f27a9a450bf6b3e81690e2
p-j01 <lele.pj01@gmail.com>
2021-06-11 12:25:56 -0400

    lint: Add shebang to build_doc script

--- doc/doxygen/build_doc.sh
+++ doc/doxygen/build_doc.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 doxygen -g
 sed -i '35s/.*/PROJECT_NAME="Dogecoin"/' Doxyfile
 echo $'INPUT= ../../src ../../ ../\nOUTPUT_DIRECTORY= ./out\nGENERATE_TREEVIEW=YES\nHTML_EXTRA_STYLESHEET=theme.css' >> Doxyfile
