f280d9f09dd4eb71f0812e7f3a3b9a166f44b8cd
Malta Micael <mmicael@gmail.com>
2021-06-12 10:45:14 -0400

    test: Set Base Version

--- test/functional/p2p_segwit.py
+++ test/functional/p2p_segwit.py
@@ -243,7 +243,7 @@ class SegWitTest(BitcoinTestFramework):
         height = self.nodes[0].getblockcount() + 1
         block_time = self.nodes[0].getblockheader(tip)["mediantime"] + 1
         block = create_block(int(tip, 16), create_coinbase(height), block_time)
-        block.nVersion = version
+        block.set_base_version(version)
         block.rehash()
         return block
 
