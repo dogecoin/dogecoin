16e9d51042d9792bb6b946dfcc502024207c0bb7
Ross Nicoll <jrn@jrn.me.uk>
2021-07-19 22:48:40 +0100

    consensus: Tweak min blocks to keep
    * Reduce minimum blocks to keep by 25% as 1,440 appeared to trip some internal threshold in Bitcoin Core.
    * Update p2p-acceptblock.py to match.

--- src/validation.h
+++ src/validation.h
@@ -200,7 +200,7 @@ extern bool fPruneMode;
 /** Number of MiB of block files that we're trying to stay below. */
 extern uint64_t nPruneTarget;
 /** Block files containing a block-height within MIN_BLOCKS_TO_KEEP of chainActive.Tip() will not be pruned. */
-static const unsigned int MIN_BLOCKS_TO_KEEP = 1440;
+static const unsigned int MIN_BLOCKS_TO_KEEP = 1152;
 
 static const signed int DEFAULT_CHECKBLOCKS = 6;
 static const unsigned int DEFAULT_CHECKLEVEL = 3;
