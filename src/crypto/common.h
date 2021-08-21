16098cb386a86192b6eeb1fa784d0fb64f9e75c6
Escanor Liones <ReverseControl@users.noreply.github.com>
2021-08-21 13:19:23 -0400

    [ header file ] Add all functions back in, this was the hack to make things compile. These functions are already defined on linux. bitcoin-config.h now takes care of the magic so it compiles as is.

--- src/crypto/common.h
+++ src/crypto/common.h
@@ -11,23 +11,23 @@
 
 #include <stdint.h>
 #include <string.h>
-//
-//#include "compat/endian.h"
-//
-//uint16_t static inline ReadLE16(const unsigned char* ptr)
-//{
-//    uint16_t x;
-//    memcpy((char*)&x, ptr, 2);
-//    return le16toh(x);
-//}
-//
-//uint32_t static inline ReadLE32(const unsigned char* ptr)
-//{
-//    uint32_t x;
-//    memcpy((char*)&x, ptr, 4);
-//    return le32toh(x);
-//}
-//
+
+#include "compat/endian.h"
+
+uint16_t static inline ReadLE16(const unsigned char* ptr)
+{
+    uint16_t x;
+    memcpy((char*)&x, ptr, 2);
+    return le16toh(x);
+}
+
+uint32_t static inline ReadLE32(const unsigned char* ptr)
+{
+    uint32_t x;
+    memcpy((char*)&x, ptr, 4);
+    return le32toh(x);
+}
+
 uint64_t static inline ReadLE64(const unsigned char* ptr)
 {
     uint64_t x;
@@ -35,31 +35,31 @@ uint64_t static inline ReadLE64(const unsigned char* ptr)
     return le64toh(x);
 }
 
-//void static inline WriteLE16(unsigned char* ptr, uint16_t x)
-//{
-//    uint16_t v = htole16(x);
-//    memcpy(ptr, (char*)&v, 2);
-//}
-//
-//void static inline WriteLE32(unsigned char* ptr, uint32_t x)
-//{
-//    uint32_t v = htole32(x);
-//    memcpy(ptr, (char*)&v, 4);
-//}
-//
-//void static inline WriteLE64(unsigned char* ptr, uint64_t x)
-//{
-//    uint64_t v = htole64(x);
-//    memcpy(ptr, (char*)&v, 8);
-//}
-//
-//uint32_t static inline ReadBE32(const unsigned char* ptr)
-//{
-//    uint32_t x;
-//    memcpy((char*)&x, ptr, 4);
-//    return be32toh(x);
-//}
-//
+void static inline WriteLE16(unsigned char* ptr, uint16_t x)
+{
+    uint16_t v = htole16(x);
+    memcpy(ptr, (char*)&v, 2);
+}
+
+void static inline WriteLE32(unsigned char* ptr, uint32_t x)
+{
+    uint32_t v = htole32(x);
+    memcpy(ptr, (char*)&v, 4);
+}
+
+void static inline WriteLE64(unsigned char* ptr, uint64_t x)
+{
+    uint64_t v = htole64(x);
+    memcpy(ptr, (char*)&v, 8);
+}
+
+uint32_t static inline ReadBE32(const unsigned char* ptr)
+{
+    uint32_t x;
+    memcpy((char*)&x, ptr, 4);
+    return be32toh(x);
+}
+
 uint64_t static inline ReadBE64(const unsigned char* ptr)
 {
     uint64_t x;
@@ -67,12 +67,12 @@ uint64_t static inline ReadBE64(const unsigned char* ptr)
     return be64toh(x);
 }
 
-//void static inline WriteBE32(unsigned char* ptr, uint32_t x)
-//{
-//    uint32_t v = htobe32(x);
-//    memcpy(ptr, (char*)&v, 4);
-//}
-//
+void static inline WriteBE32(unsigned char* ptr, uint32_t x)
+{
+    uint32_t v = htobe32(x);
+    memcpy(ptr, (char*)&v, 4);
+}
+
 void static inline WriteBE64(unsigned char* ptr, uint64_t x)
 {
     uint64_t v = htobe64(x);
