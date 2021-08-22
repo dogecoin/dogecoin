e8c1c73c65b5a91106d7682f9fdedd44cfbfc5a5
Ed Tubbs <ectubbs@gmail.com>
2021-08-22 11:02:27 -0500

    Updated intel-ipsec-mb.mk

--- depends/packages/intel-ipsec-mb.mk
+++ depends/packages/intel-ipsec-mb.mk
@@ -5,7 +5,7 @@ $(package)_file_name=v$($(package)_version).tar.gz
 $(package)_sha256_hash=03501aea472d3c8fdf8f1f207816eefeaf5e4ebbdc71d88dcb26b2519841bb74
 
 define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/remove_digest_init.patch && \
+  patch -p1 < $($(package)_patch_dir)/remove_digest_init.patch
 endef
 
 define $(package)_build_cmds
