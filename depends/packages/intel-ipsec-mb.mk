ed33f167063656ee22004ff469281182cd6a17eb
Ed Tubbs <ectubbs@gmail.com>
2021-08-21 23:42:28 -0500

    Added nasm and intel-ipsec-mb to depends Updated ci.yml

---
+++ depends/packages/intel-ipsec-mb.mk
@@ -0,0 +1,21 @@
+package=intel-ipsec-mb
+$(package)_version=1.0
+$(package)_download_path=https://github.com/intel/intel-ipsec-mb/archive/refs/tags
+$(package)_file_name=v$(package)_version).tar.gz
+$(package)_sha256_hash=03501aea472d3c8fdf8f1f207816eefeaf5e4ebbdc71d88dcb26b2519841bb74
+
+define $(package)_set_vars
+$(package)_config_opts=--disable-shared
+endef
+
+define $(package)_config_cmds
+  $($(package)_autoconf)
+endef
+
+define $(package)_build_cmds
+  $(MAKE)
+endef
+
+define $(package)_stage_cmds
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
---
+++ depends/packages/nasm.mk
@@ -0,0 +1,21 @@
+package=nasm
+$(package)_version=2.15.05
+$(package)_download_path=http://nasm.us/pub/nasm/releasebuilds/2.15.05/
+$(package)_file_name=$(package)-$($(package)_version).tar.bz2
+$(package)_sha256_hash=3c4b8339e5ab54b1bcb2316101f8985a5da50a3f9e504d43fa6f35668bee2fd0
+
+define $(package)_set_vars
+$(package)_config_opts=--disable-shared
+endef
+
+define $(package)_config_cmds
+  $($(package)_autoconf)
+endef
+
+define $(package)_build_cmds
+  $(MAKE)
+endef
+
+define $(package)_stage_cmds
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
