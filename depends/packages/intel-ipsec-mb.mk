bb84b71f7937bf0ebe4eac08f1724f09d7c2e30e
Ed <84785904+edtubbs@users.noreply.github.com>
2021-08-22 12:20:03 -0500

    Update intel-ipsec-mb.mk

--- depends/packages/intel-ipsec-mb.mk
+++ depends/packages/intel-ipsec-mb.mk
@@ -3,6 +3,7 @@ $(package)_version=1.0
 $(package)_download_path=https://github.com/intel/intel-ipsec-mb/archive/refs/tags
 $(package)_file_name=v$($(package)_version).tar.gz
 $(package)_sha256_hash=03501aea472d3c8fdf8f1f207816eefeaf5e4ebbdc71d88dcb26b2519841bb74
+$(package)_patches=remove_digest_init.patch
 
 define $(package)_preprocess_cmds
   patch -p1 < $($(package)_patch_dir)/remove_digest_init.patch
