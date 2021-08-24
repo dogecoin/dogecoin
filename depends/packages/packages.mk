185ee00bf25e07104e4ce65ebb3e031f88d5dc7b
Ed Tubbs <ectubbs@gmail.com>
2021-08-23 20:54:32 -0500

    Updated packages.mk to address macos

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,4 +1,4 @@
-packages:=boost openssl libevent zeromq intel-ipsec-mb
+packages:=boost openssl libevent zeromq
 native_packages := native_ccache
 
 qt_native_packages = native_protobuf
@@ -6,9 +6,12 @@ qt_packages = qrencode protobuf zlib
 
 qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
 qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
+ifeq ($build_os),linux)
+packages += intel-ipsec-mb
+endif
 
 qt_darwin_packages=qt
-qt_mingw32_packages=qt
+qt_mingw32_packages=qt intel-ipsec-mb
 
 wallet_packages=bdb
 
