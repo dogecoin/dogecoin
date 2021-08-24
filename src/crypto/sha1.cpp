28aded39bbb11ec36392adab40d54109efb1fa52
Ed Tubbs <ectubbs@gmail.com>
2021-08-23 19:55:36 -0500

    Fixed guard syntax

--- src/crypto/sha1.cpp
+++ src/crypto/sha1.cpp
@@ -8,9 +8,9 @@
 
 #include <string.h>
 
-#if define (__ia64__) &&
-     ((defined(__linux__) || defined(_WIN64)) &&
-     !(defined(__APPLE__)))
+#if defined(__ia64__) && \
+    (defined(__linux__) || defined(_WIN64)) && \
+    !defined(__APPLE__)
 #define USE_AVX2
 #include <intel-ipsec-mb.h>
 #endif
