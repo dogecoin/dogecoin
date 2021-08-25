4e16f4e8bbd291eb104bb3eea82c62fee92283d7
Ed Tubbs <ectubbs@gmail.com>
2021-08-25 14:24:18 -0500

    Moved nasm back to depends

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -6,8 +6,9 @@ qt_packages = qrencode protobuf zlib
 
 qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
 qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
-ifeq (($build_os),linux)
-packages += intel-ipsec-mb
+
+ifeq ($(build_os),linux)
+packages += nasm intel-ipsec-mb
 endif
 
 qt_darwin_packages=qt
