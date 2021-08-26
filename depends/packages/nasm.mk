c28ee2e9856bbe764c56eef4a1238174f65e076e
Ed Tubbs <ectubbs@gmail.com>
2021-08-25 20:17:52 -0500

    Updated nasm.mk with prefix

--- depends/packages/nasm.mk
+++ depends/packages/nasm.mk
@@ -5,7 +5,7 @@ $(package)_file_name=$(package)-$($(package)_version).tar.bz2
 $(package)_sha256_hash=3c4b8339e5ab54b1bcb2316101f8985a5da50a3f9e504d43fa6f35668bee2fd0
 
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  ./configure --prefix=$(host_prefix)
 endef
 
 define $(package)_build_cmds
