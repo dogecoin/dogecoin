fbd4ecdae172d596667fd10bbe7aea5161289158
Ed <84785904+edtubbs@users.noreply.github.com>
2021-09-07 08:48:23 -0500

    Update openssl.mk

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -58,8 +58,7 @@ $(package)_config_opts_i686_mingw32=mingw
 endef
 
 define $(package)_preprocess_cmds
-  sed -i.old "/define DATE/d" util/mkbuildinf.pl && \
-  sed -i.old "s|engines apps test|engines|" Makefile.org
+  sed -i.old "/define DATE/d" util/mkbuildinf.pl
 endef
 
 define $(package)_config_cmds
