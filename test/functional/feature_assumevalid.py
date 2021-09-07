320d27a456e16b7e6fb562129a08ec99b723cd8f
Ross Nicoll <jrn@jrn.me.uk>
2021-09-08 00:22:55 +0100

    Use set_base_version in tests
    Use set_base_version in tests in preparation for adding chain IDs to block versions.

--- test/functional/feature_assumevalid.py
+++ test/functional/feature_assumevalid.py
@@ -151,7 +151,7 @@ class AssumeValidTest(BitcoinTestFramework):
         # Bury the assumed valid block 2240 deep
         for _ in range(2240):
             block = create_block(self.tip, create_coinbase(height), self.block_time)
-            block.nVersion = 4
+            block.set_base_version(4)
             block.solve()
             self.blocks.append(block)
             self.tip = block.sha256
