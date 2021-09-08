2719c4e7067ea98c13cec6d6c1652ccb255df602
Ed <84785904+edtubbs@users.noreply.github.com>
2021-09-07 22:23:25 -0500

    Update openssl.mk

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -8,7 +8,6 @@ $(package)_sha256_hash=ecd0c6ffb493dd06707d38b14bb4d8c2288bb7033735606569d8f90f8
 define $(package)_set_vars
 $(package)_config_env=AR="$($(package)_ar)" RANLIB="$($(package)_ranlib)" CC="$($(package)_cc)"
 $(package)_config_opts=--prefix=$(host_prefix) --openssldir=$(host_prefix)/etc/openssl
-$(package)_config_opts+=no-camellia
 $(package)_config_opts+=no-capieng
 $(package)_config_opts+=no-cast
 $(package)_config_opts+=no-dso
