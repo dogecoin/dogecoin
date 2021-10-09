a4d965547e21371302616bcd000a6773f9458369
Patrick Lodder <patricklodder@users.noreply.github.com>
2021-10-09 17:17:34 +0200

    fees: set the wallet default discard threshold to 1 DOGE
    As of writing, 97% of the relay network (for me: 2251 of 2328
    peers) enforces a 1 DOGE hard dust limit making attempts to send
    smaller outputs extremely unlikely to make it through to miners.
    
    This setting is a temporary measure until a significant portion of
    the network accepts lower dust thresholds. The threshold can be
    changed by wallet users using the -discardthreshold parameter.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -57,7 +57,12 @@ static const CAmount DEFAULT_FALLBACK_FEE = RECOMMENDED_MIN_TX_FEE;
 //! -mintxfee default
 static const CAmount DEFAULT_TRANSACTION_MINFEE = RECOMMENDED_MIN_TX_FEE;
 //! -discardthreshold default
-static const CAmount DEFAULT_DISCARD_THRESHOLD = DEFAULT_DUST_LIMIT;
+/* 1.14.5: set the wallet's discard threshold to 1 DOGE because that's what 97%
+ *         of the network currently implements as the hard dust limit. This
+ *         value can be changed when a significant portion of the relay network
+ *         and miners have adopted a different hard dust limit.
+ */
+static const CAmount DEFAULT_DISCARD_THRESHOLD = COIN;
 
 //! minimum recommended increment for BIP 125 replacement txs
 /*
