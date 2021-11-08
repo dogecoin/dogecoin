e016e31a2b24449a0eea92dd564ce328d8bba693
chromatic <chromatic@wgz.org>
2021-11-07 16:17:44 -0800

    Set font for localization language selector
    See issue #2675.
    
    Because the default fonts (Comic Sans, Comic Neue) do not contain enough
    Unicode characters to render certain language and country names
    appropriately, the system font renderer has to fall back to other fonts
    when possible.
    
    Noto Sans provides sufficient coverage for Dogecoin's purposes, and it
    is available under the Open Fonts License:
    
    https://fonts.google.com/noto/specimen/Noto+Sans/about
    
    If this font is not available, the fallback behavior should proceed as
    it did before this PR.

--- src/qt/optionsdialog.cpp
+++ src/qt/optionsdialog.cpp
@@ -86,6 +86,8 @@ OptionsDialog::OptionsDialog(QWidget *parent, bool enableWallet) :
 
     /* Display elements init */
     QDir translations(":translations");
+    QFont notoFont("Noto Sans Roboto", 10);
+    ui->lang->setFont(notoFont);
 
     ui->bitcoinAtStartup->setToolTip(ui->bitcoinAtStartup->toolTip().arg(tr(PACKAGE_NAME)));
     ui->bitcoinAtStartup->setText(ui->bitcoinAtStartup->text().arg(tr(PACKAGE_NAME)));
