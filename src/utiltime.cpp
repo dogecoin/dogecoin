bd865b061dc8ae265bb729683da507afc8ffb0a8
Patrick Lodder <patricklodder@users.noreply.github.com>
2021-11-10 18:14:53 -0500

    build: remove outdated boost checks now that we require 1_60+

--- src/utiltime.cpp
+++ src/utiltime.cpp
@@ -61,20 +61,7 @@ int64_t GetLogTimeMicros()
 
 void MilliSleep(int64_t n)
 {
-
-/**
- * Boost's sleep_for was uninterruptible when backed by nanosleep from 1.50
- * until fixed in 1.52. Use the deprecated sleep method for the broken case.
- * See: https://svn.boost.org/trac/boost/ticket/7238
- */
-#if defined(HAVE_WORKING_BOOST_SLEEP_FOR)
     boost::this_thread::sleep_for(boost::chrono::milliseconds(n));
-#elif defined(HAVE_WORKING_BOOST_SLEEP)
-    boost::this_thread::sleep(boost::posix_time::milliseconds(n));
-#else
-//should never get here
-#error missing boost sleep implementation
-#endif
 }
 
 std::string DateTimeStrFormat(const char* pszFormat, int64_t nTime)
