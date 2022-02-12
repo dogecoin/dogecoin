2c7772e90622531bed6b660309a44b98b73598ba
Patrick Lodder <patricklodder@users.noreply.github.com>
2022-02-12 17:45:54 +0100

    depends: change fallback to depends.dogecoincore.org
    Changes the fallback dependency source repository from Bitcoin's
    bitcoincore.org to dogecoincore.org. Both are centrally hosted,
    as a fallback. Ideally this would be utilizing a decentralized
    protocol like IPFS, but this is harder to integrate with the CI
    tooling.
    
    Note: because some dependencies have identical filenames in their
    source URLs (currently ds_store and mac_alias) we host the target
    filename on the mirror, to prevent conflicts.

--- depends/funcs.mk
+++ depends/funcs.mk
@@ -28,10 +28,12 @@ define fetch_file_inner
     rm -rf $$($(1)_download_dir) )
 endef
 
+# Dogecoin: fetch the target filename from our mirror instead of the source,
+#           because some of the tag archives from github are identical
 define fetch_file
     ( test -f $$($(1)_source_dir)/$(4) || \
     ( $(call fetch_file_inner,$(1),$(2),$(3),$(4),$(5)) || \
-      $(call fetch_file_inner,$(1),$(FALLBACK_DOWNLOAD_PATH),$(3),$(4),$(5))))
+      $(call fetch_file_inner,$(1),$(FALLBACK_DOWNLOAD_PATH),$(4),$(4),$(5))))
 endef
 
 define int_get_build_recipe_hash
