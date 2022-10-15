330cea911f1020972113c8c3ad6a138282cd6b17
Patrick Lodder <patricklodder@users.noreply.github.com>
2022-10-15 10:56:55 +0200

    enforce explicit experimental configuration through annotation
    Introduces a new support header that exposes the macro
    EXPERIMENTAL_FEATURE that (a) allows us to clearly mark blocks
    of code as experimental (or entire files) and (b) enforces that
    none of the annotated code gets compiled if --enable-experimental
    was not configured through a static assertion.
    
    Existing experimental features are annotated:
    
    - AVX2 using the intel-ipsec-mb dependency for SHA1/256/512
    - ARMv8 intrinsics for SHA1/256
    - ARMv82 intrinsics for SHA512
    - SSE2 for scrypt

--- src/crypto/sha512.cpp
+++ src/crypto/sha512.cpp
@@ -6,6 +6,7 @@
 #include "crypto/sha512.h"
 
 #include "crypto/common.h"
+#include "support/experimental.h"
 
 #include <string.h>
 
@@ -99,6 +100,8 @@ void inline Round(uint64_t a, uint64_t b, uint64_t c, uint64_t& d, uint64_t e, u
 #endif
 
 #ifdef USE_ARMV82
+
+EXPERIMENTAL_FEATURE
 
 /* ----------------------------------------------------------------------
  * Hardware-accelerated implementation of SHA-512 using Arm NEON.
@@ -311,6 +314,7 @@ void Transform(uint64_t* s, const unsigned char* chunk)
 {
 #ifdef USE_AVX2
     // Perform SHA512 one block (Intel AVX2)
+    EXPERIMENTAL_FEATURE
     sha512_one_block_avx2(chunk, s);
 #elif USE_ARMV82
     sha512_neon_core core;
