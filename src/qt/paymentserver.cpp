728369efbf1072ae1673b9bfdeb9a602654201c1
Bertrand Jacquin <bertrand@jacquin.bzh>
2023-01-21 02:16:48 +0000

    Remove Q_FOREACH
    dogecoin fails to build with qt 5.15.8:
    
      x86_64-pc-linux-gnu-g++ -std=c++11 -DHAVE_CONFIG_H -I. -I../src/config   -I. -I./obj -I/usr/include/db5.3/ -pthread -I/usr/include -I./leveldb/include -I./leveldb/helpers/memenv   -I./secp256k1/include  -I./qt -I./qt -I./qt/forms -I./qt/forms -DQT_NO_KEYWORDS -I/usr/include/qt5/QtCore -I/usr/include/qt5 -I/usr/include/qt5/QtGui -DQT_NETWORK_LIB -I/usr/include/qt5/QtNetwork -I/usr/include/qt5/QtWidgets -DQT_PRINTSUPPORT_LIB -I/usr/include/qt5/QtPrintSupport -DQT_WIDGETS_LIB -DQT_GUI_LIB -DQT_CORE_LIB  -DQT_DBUS_LIB -I/usr/include/qt5/QtDBus -I/usr/include/qt5 -DQT_CORE_LIB -I/usr/include/qt5/QtCore    -I/usr/include/db5.3 -DHAVE_BUILD_INFO -D__STDC_FORMAT_MACROS   -fPIC -march=native -O2 -pipe -fomit-frame-pointer -c -o qt/libdogecoinqt_a-paymentserver.o `test -f 'qt/paymentserver.cpp' || echo './'`qt/paymentserver.cpp
      qt/paymentserver.cpp:556:70: error: macro "Q_FOREACH_IMPL" passed 4 arguments, but takes just 3
        556 |     Q_FOREACH(const PAIRTYPE(CScript, CAmount)& sendingTo, sendingTos) {
            |                                                                      ^
      In file included from /usr/include/qt5/QtCore/qatomic.h:41,
                       from /usr/include/qt5/QtCore/qrefcount.h:43,
                       from /usr/include/qt5/QtCore/qbytearray.h:44,
                       from /usr/include/qt5/QtCore/QByteArray:1,
                       from qt/paymentrequestplus.h:17,
                       from qt/paymentserver.h:35,
                       from qt/paymentserver.cpp:6:

--- src/qt/paymentserver.cpp
+++ src/qt/paymentserver.cpp
@@ -553,7 +553,7 @@ bool PaymentServer::processPaymentRequest(const PaymentRequestPlus& request, Sen
     QList<std::pair<CScript, CAmount> > sendingTos = request.getPayTo();
     QStringList addresses;
 
-    Q_FOREACH(const PAIRTYPE(CScript, CAmount)& sendingTo, sendingTos) {
+    for (const PAIRTYPE(CScript, CAmount)& sendingTo : sendingTos) {
         // Extract and check destination addresses
         CTxDestination dest;
         if (ExtractDestination(sendingTo.first, dest)) {
