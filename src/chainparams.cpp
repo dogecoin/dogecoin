902440dd13c199600a92b70c4bb4bdab3918d56f
David Eichel <david22782@gmail.com>
2023-09-01 00:08:35 -0400

    Revert pszTimestamp value in Genesis block. We will change this when ready to switch to the Pepecoin chain

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -51,7 +51,11 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
  */
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
-    const char* pszTimestamp = "08/25/2023 - Biden administration unveils new crypto tax reporting rules";
+    const char* pszTimestamp = "Nintondo";
+    //PEPE TODO - Change pszTimestamp swap the line above with the one below
+    //            DOING THIS WILL CHANGE THE HASH OF THE GENESIS BLOCK AND ALL SUBSEQUENT BLOCKS
+    //            ONLY DO SO WHEN WE MOVE FROM THE DOGECOIN CHAIN TO THE PEPECOIN CHAIN
+    // const char* pszTimestamp = "08/25/2023 - Biden administration unveils new crypto tax reporting rules";
     const CScript genesisOutputScript = CScript() << ParseHex("040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
