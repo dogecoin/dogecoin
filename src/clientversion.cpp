65d36688e06f3f70be694ec80e167e7a2579c951
Skylar Loomis <54447040+AjaxPop@users.noreply.github.com>
2023-09-20 10:18:29 -0400

    Update clientversion.cpp

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -42,11 +42,10 @@ const std::string CLIENT_NAME("Shibetoshi");
 #include "build.h"
 #endif
 
-//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
-#define GIT_ARCHIVE 1
+//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. $Format:%n#define GIT_ARCHIVE 1$
 #ifdef GIT_ARCHIVE
 #define GIT_COMMIT_ID "$Format:%h$"
-#define GIT_COMMIT_DATE "$Format:%h$"
+#define GIT_COMMIT_DATE "$Format:%cD$"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
