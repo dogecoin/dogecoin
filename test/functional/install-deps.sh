20d6a88eab10769d81f728ec0ccf1000e206fa27
chromatic <chromatic@wgz.org>
2023-11-23 13:39:32 -0800

    Fail CI scripts if pip installation fails

--- test/functional/install-deps.sh
+++ test/functional/install-deps.sh
@@ -5,5 +5,6 @@ export LC_ALL=C
 file=v1.0.1.tar.gz
 curl -L -O https://github.com/dogecoin/ltc-scrypt/archive/refs/tags/$file
 echo "e866ade37fb27439ae0ca32f1ee4ad32be428c1fdac9bcc988b36c68648ff0de  $file" | sha256sum -c
-python3 -m pip install $file --user
+python3 -m pip install $file --user || exit 1
 rm -rf $file
+exit 0
