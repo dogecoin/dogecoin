bf80b6d8432830a2914baf8048f6e3b358f76b66
Patrick Lodder <patricklodder@users.noreply.github.com>
2024-02-25 13:09:52 -0500

    add missing copyright headers

--- src/qt/verticallabel.h
+++ src/qt/verticallabel.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2014 The Dogecoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef VERTICALLABEL_H
 #define VERTICALLABEL_H
 
--- src/test/scrypt_tests.cpp
+++ src/test/scrypt_tests.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2011-2014 The Litecoin Core developers
+// Copyright (c) 2014 The Dogecoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #include <boost/test/unit_test.hpp>
 
 #include "crypto/scrypt.h"
