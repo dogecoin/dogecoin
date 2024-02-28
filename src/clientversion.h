fc01ebd2028e170a223e37dbad077275d6d6123c
Patrick Lodder <patricklodder@users.noreply.github.com>
2024-02-28 14:04:25 -0500

    open 1.15.0 for development

--- src/clientversion.h
+++ src/clientversion.h
@@ -16,12 +16,12 @@
 
 //! These need to be macros, as clientversion.cpp's and bitcoin*-res.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR 1
-#define CLIENT_VERSION_MINOR 14
-#define CLIENT_VERSION_REVISION 7
+#define CLIENT_VERSION_MINOR 15
+#define CLIENT_VERSION_REVISION 0
 #define CLIENT_VERSION_BUILD 0
 
 //! Set to true for release, false for prerelease or test build
-#define CLIENT_VERSION_IS_RELEASE true
+#define CLIENT_VERSION_IS_RELEASE false
 
 /**
  * Copyright year (2009-this)
