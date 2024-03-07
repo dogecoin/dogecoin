aab8209f14def3cab67273dde99d31484e1a3a62
cuinix <915115094@qq.com>
2024-03-07 13:54:32 +0800

    fix some typos in comments
    Signed-off-by: cuinix <915115094@qq.com>

--- src/test/txvalidationcache_tests.cpp
+++ src/test/txvalidationcache_tests.cpp
@@ -29,7 +29,7 @@ ToMemPool(CMutableTransaction& tx)
 
 BOOST_FIXTURE_TEST_CASE(tx_mempool_block_doublespend, TestChain240Setup)
 {
-    // Make sure skipping validation of transctions that were
+    // Make sure skipping validation of transactions that were
     // validated going into the memory pool does not allow
     // double-spends in blocks to pass validation when they should not.
 
