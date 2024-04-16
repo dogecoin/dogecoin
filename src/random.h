2e80b494ca8ace0153e17f105faa9db3604376f1
Dakoda Greaves <dakoda@xanimo.net>
2024-04-16 14:42:14 -0700

    util: Specific GetOSRandom for Linux/FreeBSD/OpenBSD
    These are available in sandboxes without access to files or
    devices. Also [they are safer and more straightforward](https://en.wikipedia.org/wiki/Entropy-supplying_system_calls)
    to use than `/dev/urandom` as reading from a file has quite a few edge
    cases:
    
    - Linux: `getrandom(buf, buflen, 0)`. [getrandom(2)](http://man7.org/linux/man-pages/man2/getrandom.2.html)
      was introduced in version 3.17 of the Linux kernel.
    - OpenBSD: `getentropy(buf, buflen)`. The [getentropy(2)](http://man.openbsd.org/cgi-bin/man.cgi/OpenBSD-current/man2/getentropy.2)
      function appeared in OpenBSD 5.6.
    - FreeBSD and NetBSD: `sysctl(KERN_ARND)`. Not sure when this was added
      but it has existed for quite a while.
    
    Alternatives:
    
    - Linux has sysctl `CTL_KERN` / `KERN_RANDOM` / `RANDOM_UUID`
      which gives 16 bytes of randomness. This may be available
      on older kernels, however [sysctl is deprecated on Linux](https://lwn.net/Articles/605392/)
      and even removed in some distros so we shouldn't use it.
    
    Add tests for `GetOSRand()`:
    
    - Test that no error happens (otherwise `RandFailure()` which aborts)
    - Test that all 32 bytes are overwritten (initialize with zeros, try multiple times)
    
    Discussion:
    
    - When to use these? Currently they are always used when available.
      Another option would be to use them only when `/dev/urandom` is not
      available. But this would mean these code paths receive less testing,
      and I'm not sure there is any reason to prefer `/dev/urandom`.
    
    Closes: #9676
    
    Cherry-picked from: 224e6eb089a0f4977d22f3803fc27e44b5e7eea5
    
    Contains squashed commit of aa09ccbb74ea9febd83ce3362238ac5339069909 squashme: comment that NUM_OS_RANDOM_BYTES should not be changed lightly

--- src/random.h
+++ src/random.h
@@ -50,4 +50,16 @@ public:
     uint32_t Rw;
 };
 
+/* Number of random bytes returned by GetOSRand.
+ * When changing this constant make sure to change all call sites, and make
+ * sure that the underlying OS APIs for all platforms support the number.
+ * (many cap out at 256 bytes).
+ */
+static const ssize_t NUM_OS_RANDOM_BYTES = 32;
+
+/** Get 32 bytes of system entropy. Do not use this in application code: use
+ * GetStrongRandBytes instead.
+ */
+void GetOSRand(unsigned char *ent32);
+
 #endif // BITCOIN_RANDOM_H
