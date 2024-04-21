440f776b8424d14d91070ec7b67db9b7fc7890bd
Matt Corallo <git@bluematt.me>
2024-04-20 18:50:32 -0700

    SyncTransaction->TxAddedToMempool/BlockConnected/Disconnected
    This simplifies fixing the wallet-returns-stale-info issue as we
    can now hold cs_wallet across an entire block instead of only
    per-tx (though we only actually do so in the next commit).
    
    This change also removes the NOT_IN_BLOCK constant in favor of only
    passing the CBlockIndex* parameter to SyncTransactions when a new
    block is being connected, instead of also when a block is being
    disconnected.
    
    This change adds a parameter to BlockConnectedDisconnected which
    lists the transactions which were removed from mempool due to
    confliction as a result of this operation. While its somewhat of a
    shame to make block-validation-logic generate a list of mempool
    changes to be included in its generated callbacks, fixing this isnt
    too hard.
    
    Further in this change-set, CValidationInterface starts listening
    to mempool directly, placing it in the middle and giving it a bit
    of logic to know how to route notifications from block-validation,
    mempool, etc (though not listening for conflicted-removals yet).
    
    Cherry-picked from: 461e49fee2935b1eb4d4ea7bae3023e655c0a6d8
    
    [dogecoin: the boost::bind placeholders here are qualified as
    boost::placeholders::_N for Boost >= 1.73, matching this tree's
    convention; see dogecoin #1655.]

--- src/validationinterface.cpp
+++ src/validationinterface.cpp
@@ -16,58 +16,33 @@ CMainSignals& GetMainSignals()
 }
 
 void RegisterValidationInterface(CValidationInterface* pwalletIn) {
-    g_signals.UpdatedBlockTip.connect(boost::bind(&CValidationInterface::UpdatedBlockTip,
-                                                  pwalletIn, boost::placeholders::_1,
-                                                  boost::placeholders::_2,
-                                                  boost::placeholders::_3));
-    g_signals.SyncTransaction.connect(boost::bind(&CValidationInterface::SyncTransaction,
-                                                  pwalletIn, boost::placeholders::_1,
-                                                  boost::placeholders::_2,
-                                                  boost::placeholders::_3));
-    g_signals.UpdatedTransaction.connect(boost::bind(&CValidationInterface::UpdatedTransaction,
-                                                     pwalletIn, boost::placeholders::_1));
-    g_signals.SetBestChain.connect(boost::bind(&CValidationInterface::SetBestChain,
-                                               pwalletIn, boost::placeholders::_1));
-    g_signals.Broadcast.connect(boost::bind(&CValidationInterface::ResendWalletTransactions,
-                                            pwalletIn, boost::placeholders::_1, boost::placeholders::_2));
-    g_signals.BlockChecked.connect(boost::bind(&CValidationInterface::BlockChecked,
-                                               pwalletIn, boost::placeholders::_1,
-                                               boost::placeholders::_2));
-    g_signals.ScriptForMining.connect(boost::bind(&CValidationInterface::GetScriptForMining,
-                                                  pwalletIn, boost::placeholders::_1));
-    g_signals.BlockFound.connect(boost::bind(&CValidationInterface::ResetRequestCount,
-                                             pwalletIn, boost::placeholders::_1));
-    g_signals.NewPoWValidBlock.connect(boost::bind(&CValidationInterface::NewPoWValidBlock,
-                                                   pwalletIn, boost::placeholders::_1,
-                                                   boost::placeholders::_2));
+    g_signals.UpdatedBlockTip.connect(boost::bind(&CValidationInterface::UpdatedBlockTip, pwalletIn, boost::placeholders::_1, boost::placeholders::_2, boost::placeholders::_3));
+    g_signals.TransactionAddedToMempool.connect(boost::bind(&CValidationInterface::TransactionAddedToMempool, pwalletIn, boost::placeholders::_1));
+    g_signals.BlockConnected.connect(boost::bind(&CValidationInterface::BlockConnected, pwalletIn, boost::placeholders::_1, boost::placeholders::_2, boost::placeholders::_3));
+    g_signals.BlockDisconnected.connect(boost::bind(&CValidationInterface::BlockDisconnected, pwalletIn, boost::placeholders::_1));
+    g_signals.UpdatedTransaction.connect(boost::bind(&CValidationInterface::UpdatedTransaction, pwalletIn, boost::placeholders::_1));
+    g_signals.SetBestChain.connect(boost::bind(&CValidationInterface::SetBestChain, pwalletIn, boost::placeholders::_1));
+    g_signals.Inventory.connect(boost::bind(&CValidationInterface::Inventory, pwalletIn, boost::placeholders::_1));
+    g_signals.Broadcast.connect(boost::bind(&CValidationInterface::ResendWalletTransactions, pwalletIn, boost::placeholders::_1, boost::placeholders::_2));
+    g_signals.BlockChecked.connect(boost::bind(&CValidationInterface::BlockChecked, pwalletIn, boost::placeholders::_1, boost::placeholders::_2));
+    g_signals.ScriptForMining.connect(boost::bind(&CValidationInterface::GetScriptForMining, pwalletIn, boost::placeholders::_1));
+    g_signals.BlockFound.connect(boost::bind(&CValidationInterface::ResetRequestCount, pwalletIn, boost::placeholders::_1));
+    g_signals.NewPoWValidBlock.connect(boost::bind(&CValidationInterface::NewPoWValidBlock, pwalletIn, boost::placeholders::_1, boost::placeholders::_2));
 }
 
 void UnregisterValidationInterface(CValidationInterface* pwalletIn) {
-    g_signals.BlockFound.disconnect(boost::bind(&CValidationInterface::ResetRequestCount,
-                                                pwalletIn, boost::placeholders::_1));
-    g_signals.ScriptForMining.disconnect(boost::bind(&CValidationInterface::GetScriptForMining,
-                                                     pwalletIn, boost::placeholders::_1));
-    g_signals.BlockChecked.disconnect(boost::bind(&CValidationInterface::BlockChecked,
-                                                  pwalletIn, boost::placeholders::_1,
-                                                  boost::placeholders::_2));
-    g_signals.Broadcast.disconnect(boost::bind(&CValidationInterface::ResendWalletTransactions,
-                                               pwalletIn, boost::placeholders::_1,
-                                               boost::placeholders::_2));
-    g_signals.SetBestChain.disconnect(boost::bind(&CValidationInterface::SetBestChain,
-                                                  pwalletIn, boost::placeholders::_1));
-    g_signals.UpdatedTransaction.disconnect(boost::bind(&CValidationInterface::UpdatedTransaction,
-                                                        pwalletIn, boost::placeholders::_1));
-    g_signals.SyncTransaction.disconnect(boost::bind(&CValidationInterface::SyncTransaction,
-                                                     pwalletIn, boost::placeholders::_1,
-                                                     boost::placeholders::_2,
-                                                     boost::placeholders::_3));
-    g_signals.UpdatedBlockTip.disconnect(boost::bind(&CValidationInterface::UpdatedBlockTip,
-                                         pwalletIn, boost::placeholders::_1,
-                                         boost::placeholders::_2,
-                                         boost::placeholders::_3));
-    g_signals.NewPoWValidBlock.disconnect(boost::bind(&CValidationInterface::NewPoWValidBlock,
-                                          pwalletIn, boost::placeholders::_1,
-                                          boost::placeholders::_2));
+    g_signals.BlockFound.disconnect(boost::bind(&CValidationInterface::ResetRequestCount, pwalletIn, boost::placeholders::_1));
+    g_signals.ScriptForMining.disconnect(boost::bind(&CValidationInterface::GetScriptForMining, pwalletIn, boost::placeholders::_1));
+    g_signals.BlockChecked.disconnect(boost::bind(&CValidationInterface::BlockChecked, pwalletIn, boost::placeholders::_1, boost::placeholders::_2));
+    g_signals.Broadcast.disconnect(boost::bind(&CValidationInterface::ResendWalletTransactions, pwalletIn, boost::placeholders::_1, boost::placeholders::_2));
+    g_signals.Inventory.disconnect(boost::bind(&CValidationInterface::Inventory, pwalletIn, boost::placeholders::_1));
+    g_signals.SetBestChain.disconnect(boost::bind(&CValidationInterface::SetBestChain, pwalletIn, boost::placeholders::_1));
+    g_signals.UpdatedTransaction.disconnect(boost::bind(&CValidationInterface::UpdatedTransaction, pwalletIn, boost::placeholders::_1));
+    g_signals.TransactionAddedToMempool.disconnect(boost::bind(&CValidationInterface::TransactionAddedToMempool, pwalletIn, boost::placeholders::_1));
+    g_signals.BlockConnected.disconnect(boost::bind(&CValidationInterface::BlockConnected, pwalletIn, boost::placeholders::_1, boost::placeholders::_2, boost::placeholders::_3));
+    g_signals.BlockDisconnected.disconnect(boost::bind(&CValidationInterface::BlockDisconnected, pwalletIn, boost::placeholders::_1));
+    g_signals.UpdatedBlockTip.disconnect(boost::bind(&CValidationInterface::UpdatedBlockTip, pwalletIn, boost::placeholders::_1, boost::placeholders::_2, boost::placeholders::_3));
+    g_signals.NewPoWValidBlock.disconnect(boost::bind(&CValidationInterface::NewPoWValidBlock, pwalletIn, boost::placeholders::_1, boost::placeholders::_2));
 }
 
 void UnregisterAllValidationInterfaces() {
@@ -75,9 +50,12 @@ void UnregisterAllValidationInterfaces() {
     g_signals.ScriptForMining.disconnect_all_slots();
     g_signals.BlockChecked.disconnect_all_slots();
     g_signals.Broadcast.disconnect_all_slots();
+    g_signals.Inventory.disconnect_all_slots();
     g_signals.SetBestChain.disconnect_all_slots();
     g_signals.UpdatedTransaction.disconnect_all_slots();
-    g_signals.SyncTransaction.disconnect_all_slots();
+    g_signals.TransactionAddedToMempool.disconnect_all_slots();
+    g_signals.BlockConnected.disconnect_all_slots();
+    g_signals.BlockDisconnected.disconnect_all_slots();
     g_signals.UpdatedBlockTip.disconnect_all_slots();
     g_signals.NewPoWValidBlock.disconnect_all_slots();
 }
