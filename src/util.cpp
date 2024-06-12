1d84f4f853c4dd8e9e5267d0fce0706d33422e36
Patrick Lodder <patricklodder@users.noreply.github.com>
2024-06-12 05:21:53 -0400

    fs: use path::is_absolute() instead of path::is_complete()
    path::is_complete() has been deprecated since boost::filesystem
    version 3, which was introduced with boost 1.44.0, and removed in
    1.85.0. The minimum supported boost version has been 1.60.0 since
    Dogecoin Core release v1.14.3, so this can be safely replaced.

--- src/util.cpp
+++ src/util.cpp
@@ -588,7 +588,7 @@ const fs::path &GetBackupDir()
 fs::path GetConfigFile(const std::string& confPath)
 {
     fs::path pathConfigFile(confPath);
-    if (!pathConfigFile.is_complete())
+    if (!pathConfigFile.is_absolute())
         pathConfigFile = GetDataDir(false) / pathConfigFile;
 
     return pathConfigFile;
@@ -624,7 +624,7 @@ void ReadConfigFile(const std::string& confPath)
 fs::path GetPidFile()
 {
     fs::path pathPidFile(GetArg("-pid", BITCOIN_PID_FILENAME));
-    if (!pathPidFile.is_complete()) pathPidFile = GetDataDir() / pathPidFile;
+    if (!pathPidFile.is_absolute()) pathPidFile = GetDataDir() / pathPidFile;
     return pathPidFile;
 }
 
