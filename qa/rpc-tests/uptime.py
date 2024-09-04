70f8997bf05c0016b798ce9e9ca0a108d6e8eb19
nformant <54002590+nformant1@users.noreply.github.com>
2024-09-04 23:49:34 +0200

    add uptime test

--- qa/rpc-tests/uptime.py
+++ qa/rpc-tests/uptime.py
@@ -10,7 +10,7 @@ Test corresponds to code in rpc/server.cpp.
 import time
 
 from test_framework.test_framework import BitcoinTestFramework
-
+from test_framework.util import *
 
 class UptimeTest(BitcoinTestFramework):
     def __init__(self):
@@ -18,6 +18,12 @@ class UptimeTest(BitcoinTestFramework):
 
         self.num_nodes = 1
         self.setup_clean_chain = True
+
+    def setup_network(self, split=False):
+        self.nodes = []
+        self.nodes.append(start_node(0, self.options.tmpdir, []))
+        self.is_network_split=False
+        self.sync_all()
 
     def run_test(self):
         self._test_uptime()
