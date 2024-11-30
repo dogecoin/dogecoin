xaya/xaya

Check auxpow PoW before the auxpow itself.
This reverses the order in which the auxpow structure and the PoW on it
are verified.  By checking the PoW first, we make it harder for random
nodes to pass data of their choosing into CAuxPow::check.

src/auxpow.cpp
@@ -58,6 +58,10 @@ CAuxPow::check (const uint256& hashAuxBlock, int nChainId,
           != parentBlock.hashMerkleRoot)
         return error("Aux POW merkle root incorrect");
 
+    // Check that there is at least one input.
+    if (coinbaseTx->vin.empty())
+        return error("Aux POW coinbase has no inputs");
+
     const CScript script = coinbaseTx->vin[0].scriptSig;
 
     // Check that the same work is not submitted twice to our chain.

dogecoin/dogecoin

Cherry-picked from: namecoin/namecoin-core@b6e3241b
Conflicts resolved:
- CAuxPow member name mismatch
- source code from validation.cpp resides in dogecoin.cpp

src/auxpow.cpp
@@ -101,6 +101,10 @@ CAuxPow::check(const uint256& hashAuxBlock, int nChainId,
           != parentBlock.hashMerkleRoot)
         return error("Aux POW merkle root incorrect");
 
+    // Check that there is at least one input.
+    if (tx->vin.empty())
+        return error("Aux POW coinbase has no inputs");
+
     const CScript script = tx->vin[0].scriptSig;
 
     // Check that the same work is not submitted twice to our chain.