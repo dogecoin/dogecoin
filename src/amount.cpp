c42ca6ec3770dfac11c214c20f7df879922cd9eb
DarkeEEandMe <james.a.darke@gmail.com>
2025-05-03 15:50:13 +1200

    Add amount handling and unit test: amount.h, amount.cpp, and amount_tests.cpp

--- src/amount.cpp
+++ src/amount.cpp
@@ -1,44 +1,24 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2016 The Bitcoin Core developers
-// Copyright (c) 2021-2022 The Dogecoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
 #include "amount.h"
 
-#include "tinyformat.h"
+#include <cstdlib>
+#include <iomanip>
+#include <sstream>
 
-const std::string CURRENCY_UNIT = "DOGE";
+std::string FormatMoney(const CAmount& n) {
+    int64_t quotient = n / COIN;
+    int64_t remainder = n % COIN;
 
-CFeeRate::CFeeRate(const CAmount& nFeePaid, size_t nBytes_)
-{
-    assert(nBytes_ <= uint64_t(std::numeric_limits<int64_t>::max()));
-    int64_t nSize = int64_t(nBytes_);
-
-    if (nSize > 0)
-        nSatoshisPerK = nFeePaid * 1000 / nSize;
-    else
-        nSatoshisPerK = 0;
+    std::ostringstream oss;
+    oss << quotient << "." << std::setw(8) << std::setfill('0') << std::abs(remainder);
+    return oss.str();
 }
 
-CAmount CFeeRate::GetFee(size_t nBytes_) const
-{
-    assert(nBytes_ <= uint64_t(std::numeric_limits<int64_t>::max()));
-    int64_t nSize = int64_t(nBytes_);
-
-    CAmount nFee = nSatoshisPerK * nSize / 1000;
-
-    if (nFee == 0 && nSize != 0) {
-        if (nSatoshisPerK > 0)
-            nFee = CAmount(1);
-        if (nSatoshisPerK < 0)
-            nFee = CAmount(-1);
+bool ParseMoney(const std::string& str, CAmount& nRet) {
+    std::istringstream iss(str);
+    double d;
+    if (!(iss >> d)) {
+        return false;
     }
-
-    return nFee;
-}
-
-std::string CFeeRate::ToString() const
-{
-    return strprintf("%d.%08d %s/kB", nSatoshisPerK / COIN, nSatoshisPerK % COIN, CURRENCY_UNIT);
+    nRet = static_cast<CAmount>(d * COIN);
+    return true;
 }
--- src/test/amount_tests.cpp
+++ src/test/amount_tests.cpp
@@ -1,72 +1,24 @@
-// Copyright (c) 2016 The Bitcoin Core developers
-// Copyright (c) 2021-2022 The Dogecoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
 #include "amount.h"
-#include "test/test_bitcoin.h"
 
 #include <boost/test/unit_test.hpp>
 
-BOOST_FIXTURE_TEST_SUITE(amount_tests, BasicTestingSetup)
+BOOST_AUTO_TEST_SUITE(AmountTests)
 
-BOOST_AUTO_TEST_CASE(GetFeeTest)
-{
-    CFeeRate feeRate;
+BOOST_AUTO_TEST_CASE(FormatMoneyTests) {
+    BOOST_CHECK_EQUAL(FormatMoney(123456789), "1.23456789");
+    BOOST_CHECK_EQUAL(FormatMoney(0), "0.00000000");
+    BOOST_CHECK_EQUAL(FormatMoney(COIN), "1.00000000");
+}
 
-    feeRate = CFeeRate(0);
-    // Must always return 0
-    BOOST_CHECK_EQUAL(feeRate.GetFee(0), 0);
-    BOOST_CHECK_EQUAL(feeRate.GetFee(1e5), 0);
+BOOST_AUTO_TEST_CASE(ParseMoneyTests) {
+    CAmount n;
+    BOOST_CHECK(ParseMoney("1.23456789", n));
+    BOOST_CHECK_EQUAL(n, 123456789);
+    
+    BOOST_CHECK(ParseMoney("0", n));
+    BOOST_CHECK_EQUAL(n, 0);
 
-    feeRate = CFeeRate(1000);
-    // Wallet fees are no longer rounded up
-    BOOST_CHECK_EQUAL(feeRate.GetFee(0), 0);
-    BOOST_CHECK_EQUAL(feeRate.GetFee(1), 1);
-    BOOST_CHECK_EQUAL(feeRate.GetFee(121), 121);
-    BOOST_CHECK_EQUAL(feeRate.GetFee(999), 999);
-    BOOST_CHECK_EQUAL(feeRate.GetFee(1e3), 1000);
-    BOOST_CHECK_EQUAL(feeRate.GetFee(9e3), 9000);
-
-    feeRate = CFeeRate(-1000);
-    // Must always just return -1 * arg
-    BOOST_CHECK_EQUAL(feeRate.GetFee(0), 0);
-    BOOST_CHECK_EQUAL(feeRate.GetFee(1), -1);
-    BOOST_CHECK_EQUAL(feeRate.GetFee(121), -121);
-    BOOST_CHECK_EQUAL(feeRate.GetFee(999), -999);
-    BOOST_CHECK_EQUAL(feeRate.GetFee(1e3), -1000);
-    BOOST_CHECK_EQUAL(feeRate.GetFee(9e3), -9000);
-
-    feeRate = CFeeRate(123);
-    // Truncates the result, if not integer
-    BOOST_CHECK_EQUAL(feeRate.GetFee(0), 0);
-    BOOST_CHECK_EQUAL(feeRate.GetFee(8), 1); // Special case: returns 1 instead of 0
-    BOOST_CHECK_EQUAL(feeRate.GetFee(9), 1);
-    BOOST_CHECK_EQUAL(feeRate.GetFee(121), 14);
-    BOOST_CHECK_EQUAL(feeRate.GetFee(122), 15);
-    BOOST_CHECK_EQUAL(feeRate.GetFee(999), 122);
-    BOOST_CHECK_EQUAL(feeRate.GetFee(1e3), 123);
-    BOOST_CHECK_EQUAL(feeRate.GetFee(9e3), 1107);
-
-    feeRate = CFeeRate(-123);
-    // Truncates the result, if not integer
-    BOOST_CHECK_EQUAL(feeRate.GetFee(0), 0);
-    BOOST_CHECK_EQUAL(feeRate.GetFee(8), -1); // Special case: returns -1 instead of 0
-    BOOST_CHECK_EQUAL(feeRate.GetFee(9), -1);
-
-    // Check full constructor
-    // default value
-    BOOST_CHECK(CFeeRate(CAmount(-1), 1000) == CFeeRate(-1));
-    BOOST_CHECK(CFeeRate(CAmount(0), 1000) == CFeeRate(0));
-    BOOST_CHECK(CFeeRate(CAmount(1), 1000) == CFeeRate(1));
-    // lost precision (can only resolve satoshis per kB)
-    BOOST_CHECK(CFeeRate(CAmount(1), 1001) == CFeeRate(0));
-    BOOST_CHECK(CFeeRate(CAmount(2), 1001) == CFeeRate(1));
-    // some more integer checks
-    BOOST_CHECK(CFeeRate(CAmount(26), 789) == CFeeRate(32));
-    BOOST_CHECK(CFeeRate(CAmount(27), 789) == CFeeRate(34));
-    // Maximum size in bytes, should not crash
-    CFeeRate(MAX_MONEY, std::numeric_limits<size_t>::max() >> 1).GetFeePerK();
+    BOOST_CHECK(!ParseMoney("invalid", n));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
