69a581c8f93b5cae26761e5831fe0f2545bb0aa0
nformant <54002590+nformant1@users.noreply.github.com>
2025-06-01 21:18:53 +0200

    added missing restore between line 269 and 270

--- src/rpc/rawtransaction.cpp
+++ src/rpc/rawtransaction.cpp
@@ -266,6 +266,7 @@ UniValue getrawtransaction(const JSONRPCRequest& request)
 
 
     UniValue result(UniValue::VOBJ);
+    result.pushKV("hex", strHex);
     if (blockindex) result.pushKV("in_active_chain", in_active_chain);
     TxToJSON(*tx, hash_block, result);
     return result;
