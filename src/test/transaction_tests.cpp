fe01d25dbece78c87f5842270b27720d6a95a73e
Piotr Zajczkowski <p.m.zajaczkowski@gmail.com>
2026-05-07 23:31:53 +0200

    Remove duplicate #include lines in three source files
    Remove second occurrences of duplicate #include directives in:
    - src/test/transaction_tests.cpp: <boost/assign/list_of.hpp>
    - src/httprpc.cpp: "utilstrencodings.h"
    - src/qt/rpcconsole.cpp: "bantablemodel.h" and <QThread>
    
    A duplicate was also found in src/leveldb/port/port_stdcxx.h, but
    that file is part of the vendored LevelDB library and is left unchanged.
    
    No behavioral change.

--- src/test/transaction_tests.cpp
+++ src/test/transaction_tests.cpp
@@ -28,7 +28,6 @@
 #include <boost/algorithm/string/split.hpp>
 #include <boost/assign/list_of.hpp>
 #include <boost/test/unit_test.hpp>
-#include <boost/assign/list_of.hpp>
 #include <boost/foreach.hpp>
 
 #include <univalue.h>
