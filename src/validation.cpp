9ef25fc7ba3869fc80e8619113572baef9d023a0
xanimo <bluezr@dogecoin.com>
2026-07-25 21:17:48 -0700

    consensus: finish the IsPruned->IsSpent rename in CheckTxInputs
    The preceding scripted-diff renamed Coin::IsPruned to IsSpent, but the
    cherry-picked diff did not reach the CheckTxInputs coinbase-maturity assert
    in Dogecoin's validation.cpp. Rename the remaining call so it builds.

--- src/validation.cpp
+++ src/validation.cpp
@@ -1368,7 +1368,7 @@ bool CheckTxInputs(const CChainParams& params, const CTransaction& tx, CValidati
         {
             const COutPoint &prevout = tx.vin[i].prevout;
             const Coin& coin = inputs.AccessCoin(prevout);
-            assert(!coin.IsPruned());
+            assert(!coin.IsSpent());
 
             // If prev is coinbase, check that it's matured
             if (coin.IsCoinBase()) {
